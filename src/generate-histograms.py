2c20b39f348046a62e2699e3f4b82955d007bd4f
James Avery <avery@diku.dk>
2020-11-15 12:47:09 +0100

    Fixed bug in histogram generation

--- src/generate-histograms.py
+++ src/generate-histograms.py
@@ -1,44 +1,58 @@
-import numpy as np;
-import numpy.ma as ma;
-import matplotlib.pyplot as plt;
-from scipy import ndimage as ndi;
-import scipy;
-import skimage.feature as feat;
-import skimage.morphology as morf;
-import skimage.filters as filt;
-import skimage.draw as drameasurew
-import skimage.measure as measure
-from joblib import Parallel, delayed
-import multiprocessing;
+import jax
+import numpy as np
+import jax.numpy as jp
+import h5py
+from functools import partial
+jax.config.update("jax_enable_x64", True)
 
+from static_constants import *;
 from esrf_read import *;
 import glob;
 import sys;
 
-num_cores = multiprocessing.cpu_count();
+sample, nbins, chunk_length, hdf5_root = sys.argv[1:];
 
-dataroot = sys.argv[1];
-#TODO: Less greasy
-nbins=65536
-bin_edges = np.linspace(-4, 12, nbins+1);
+nbins, chunk_length = int(nbins), int(chunk_length);
+bin_edges = np.linspace(0, 2**16-1, nbins+1);
 
-with open(dataroot+"/xmlfiles-pag.txt") as f:
-    xmlfiles = [x.rstrip('\n') for x in f.readlines()]
+h5_msb = h5py.File(f"{hdf5_root}/hdf5-byte/scale/1x/{sample}.h5",'r');
+h5_lsb = h5py.File(f"{hdf5_root}/hdf5-byte/lsb/1x/{sample}.h5",'r');
 
-# def frame_histogram(xml,i,bin_edges):
-#     print("Reading data frame",i)                
-#     meta,frame  = esrf_edf_n_to_npy(xml,i);
-#     print("Calculating histogram for frame",i)        
-#     return np.histogram(frame.compressed(),bins=bin_edges)[0];            
 
-for i in range(len(xmlfiles)):
-    print("\n\n",i,xmlfiles[i]);
+voxels_hi = h5_msb['voxels']
+voxels_lo = h5_lsb['voxels']
+subvolume_dimensions = h5_msb['subvolume_dimensions'];
 
-    xml = esrf_read_xml(dataroot+"/"+xmlfiles[i]);
+(Nz,Ny,Nx) = voxels_hi.shape
 
-    hcounts,bin_edges = progressive_histogram(xml,nbins=nbins,bin_edges=bin_edges,num_cores=num_cores);
+hist_z = np.zeros((Nz,nbins),dtype=jp.uint32);
+hist_y = np.zeros((Ny,nbins),dtype=jp.uint32);
+hist_x = np.zeros((Nx,nbins),dtype=jp.uint32);
 
-    print("Storing result")
-    filename = os.path.splitext(os.path.basename(xmlfiles[i]))[0]+'.npz';
-    np.savez_compressed(dataroot+"/z-histograms/"+filename,hcounts=hcounts,bin_edges=bin_edges,dataroot=dataroot,xmlfile=xmlfiles[i]);
+def merge_short(chunk_hi,chunk_lo):
+    hi = chunk_hi.astype(jp.uint16)
+    lo = chunk_lo.astype(jp.uint16)
+    return (hi<<8) | lo
 
+merge_short_jit = jax.jit(merge_short)
+
+for z in range(0,Nz,chunk_length):
+    chunk_end = min(z+chunk_length,Nz);
+    print(f"Reading and merging slice {z} to {z+chunk_length}")    
+    chunk = merge_short_jit(voxels_hi[z:chunk_end], voxels_lo[z:chunk_end])
+    print("Calculating x-histograms");
+    chunk_x = jp.transpose(chunk,(2,0,1))
+    print(chunk_x.shape, hist_x.shape)
+    hist_x[:] += jax.vmap(lambda x: jp.histogram(x,bin_edges)[0])(chunk_x)
+    del chunk_x    
+    print("Calculating y-histograms");
+    chunk_y = jp.transpose(chunk,(1,0,2))
+    hist_y[:] += jax.vmap(lambda x: jp.histogram(x,bin_edges)[0])(chunk_y)
+    del chunk_y
+    print("Calculating z-histograms");
+    hist_z[z:chunk_end] = jax.vmap(lambda x: jp.histogram(x,bin_edges)[0])(chunk)
+
+
+np.save(f"{hdf5_root}/processed/z-histograms/{sample}.npy",hist_z)
+np.save(f"{hdf5_root}/processed/y-histograms/{sample}.npy",hist_y)
+np.save(f"{hdf5_root}/processed/x-histograms/{sample}.npy",hist_x)
