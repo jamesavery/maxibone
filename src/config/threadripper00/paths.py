dbea1d5fc2fc39f584308d62fbbbf73e636a1d4e
James Avery <avery@diku.dk>
2020-11-20 10:54:31 +0100

    Added configuration directory

---
+++ src/config/threadripper00/paths.py
@@ -0,0 +1,4 @@
+hdf5_root      = "/data/MAXIBONE/Goats/tomograms"
+esrf_implants_root  = "/home/avery/erda/XNS/XrayImaging/MiG/manjula.esci.nbi.dk.2_localhost/esrf_dental_implants_april_2013/"
+esrf_granules_root  = "/home/avery/erda/XNS/XrayImaging/MiG/manjula.esci.nbi.dk.2_localhost/esrf_dental_granules_july_2012/"
+
