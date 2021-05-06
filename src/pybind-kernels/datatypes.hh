70837fc76d4b36624695ee44a0e20bc8a754373f
James Avery <avery@nbi.dk>
2021-05-06 09:43:55 +0200

    Missing commit of datatypes.hh

--- src/pybind-kernels/datatypes.hh
+++ src/pybind-kernels/datatypes.hh
@@ -10,20 +10,20 @@ struct plane_t {
   array<real_t,3> cm, u_axis, v_axis;
 };
 
-template <typename T> struct ndarray_input {
+template <typename T> struct input_ndarray {
   const T *data;
   const vector<ssize_t> shape;
 
-  ndarray_input(const T *data, const vector<ssize_t> &shape): data(data), shape(shape) {}
-  ndarray_input(const void *data, const vector<ssize_t> &shape): data(static_cast<const T*>(data)), shape(shape) {}  
+  input_ndarray(const T *data, const vector<ssize_t> &shape): data(data), shape(shape) {}
+  input_ndarray(const void *data, const vector<ssize_t> &shape): data(static_cast<const T*>(data)), shape(shape) {}  
 };
 
-template <typename T> struct ndarray_output {
+template <typename T> struct output_ndarray {
   T *data;
   const vector<ssize_t> shape;
 
-  ndarray_output(T *data, const vector<ssize_t> &shape): data(data), shape(shape) {}
-  ndarray_output(void *data, const vector<ssize_t> &shape): data(static_cast<T*>(data)), shape(shape) {}    
+  output_ndarray(T *data, const vector<ssize_t> &shape): data(data), shape(shape) {}
+  output_ndarray(void *data, const vector<ssize_t> &shape): data(static_cast<T*>(data)), shape(shape) {}    
 };
 
 
