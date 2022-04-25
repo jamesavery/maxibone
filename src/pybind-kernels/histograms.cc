661eb95593c57ebbb275dfd5a3144c74e737e39c
Carl Johnsen <cjjohnsen@nbi.ku.dk>
2022-04-25 16:09:48 +0200

    Preliminary commit before working on field histograms

--- src/pybind-kernels/histograms.cc
+++ src/pybind-kernels/histograms.cc
@@ -160,7 +160,7 @@ void axis_histogram_par_cpu(const py::array_t<voxel_type> np_voxels,
                 y_bins[y*voxel_bins + i] = tmp[i];
         }
 
-        // r_bins
+        // z_bins
         #pragma omp for nowait
         for (uint64_t z = 0; z < Nz; z++) {
             // Init
@@ -190,7 +190,7 @@ void axis_histogram_par_cpu(const py::array_t<voxel_type> np_voxels,
                 z_bins[z*voxel_bins + i] = tmp[i];
         }
 
-        // z_bins
+        // r_bins
         #pragma omp for nowait collapse(2)
         for (uint64_t y = 0; y < Ny; y++) {
             for (uint64_t x = 0; x < Nx; x++) {
@@ -436,59 +436,57 @@ void axis_histogram_seq_cpu(const py::array_t<voxel_type> np_voxels,
 }
 
 // TODO: Allow field to be lower resolution than voxel data
-void field_histogram(const py::array_t<voxel_type> np_voxels,
+void field_histogram_seq_cpu(const py::array_t<voxel_type> np_voxels,
 		     const py::array_t<field_type> np_field,
 		     py::array_t<uint64_t> &np_bins,
-		     const double vmin, const double vmax
-	     )
-{
-  py::buffer_info
-    voxels_info = np_voxels.request(),
-    field_info = np_field.request(),
-    bins_info = np_bins.request();
+		     const double vmin, const double vmax) {
+    py::buffer_info
+        voxels_info = np_voxels.request(),
+        field_info = np_field.request(),
+        bins_info = np_bins.request();
 
-  const uint64_t
-    image_length = voxels_info.size,
-    field_length = field_info.size,
-    field_bins   = bins_info.shape[0],
-    voxel_bins   = bins_info.shape[1];
+    const uint64_t
+        image_length = voxels_info.size,
+        field_length = field_info.size,
+        field_bins   = bins_info.shape[0],
+        voxel_bins   = bins_info.shape[1];
 
-  const uint64_t
-    nZ = voxels_info.shape[0], nY = voxels_info.shape[1], nX = voxels_info.shape[2],
-    nz = field_info.shape[0],  ny = field_info.shape[1],  nx = field_info.shape[2];
+    const uint64_t
+        nZ = voxels_info.shape[0], nY = voxels_info.shape[1], nX = voxels_info.shape[2],
+        nz = field_info.shape[0],  ny = field_info.shape[1],  nx = field_info.shape[2];
 
-  double dz = nz/((double)nZ), dy = ny/((double)nY), dx = nx/((double)nX);
+    double dz = nz/((double)nZ), dy = ny/((double)nY), dx = nx/((double)nX);
 
-  const voxel_type *voxels = static_cast<voxel_type*>(voxels_info.ptr);
-  const field_type *field  = static_cast<field_type*>(field_info.ptr);
-  uint64_t *bins = static_cast<uint64_t*>(bins_info.ptr);
+    const voxel_type *voxels = static_cast<voxel_type*>(voxels_info.ptr);
+    const field_type *field  = static_cast<field_type*>(field_info.ptr);
+    uint64_t *bins = static_cast<uint64_t*>(bins_info.ptr);
 
-  uint64_t I=0;
+    uint64_t I=0;
 
-  float fmin=1e6, fmax=0;	// TODO: inf, -inf
-  for(uint64_t i=0;i<field_length;i++){
-    fmin = field[i]>0? min(field[i],fmin) : fmin; // TODO: Should we really mask field zeros?
-    fmax = max(field[i],fmax);
-  }
+    float fmin=1e6, fmax=0;	// TODO: inf, -inf
+    for (uint64_t i = 0; i < field_length; i++) {
+        fmin = field[i] > 0 ? min(field[i], fmin) : fmin; // TODO: Should we really mask field zeros?
+        fmax = max(field[i], fmax);
+    }
 
   //#pragma omp parallel for private(I) reduction(+:bins[:field_bins*voxel_bins])
-  for(I=0;I<image_length;I++){
-    int64_t voxel_index = floor(static_cast<double>(voxel_bins-1) * ((voxels[I] - vmin)/(vmax - vmin)) );
+    for (I = 0; I < image_length; I++) {
+        int64_t voxel_index = floor(static_cast<double>(voxel_bins-1) * ((voxels[I] - vmin)/(vmax - vmin)) );
 
-    // What are the X,Y,Z indices corresponding to voxel basearray index I?
-    uint64_t X = I % nX, Y = (I / nX) % nY, Z = I / (nX*nY);
+        // What are the X,Y,Z indices corresponding to voxel basearray index I?
+        uint64_t X = I % nX, Y = (I / nX) % nY, Z = I / (nX*nY);
 
-    // And what are the corresponding x,y,z coordinates into the field array, and field basearray index i?
-    // TODO: Sample 2x2x2 volume?
-    uint64_t x = floor(X*dx), y = floor(Y*dy), z = floor(Z*dz);
-    uint64_t i = z*ny*nx + y*nx + x;
+        // And what are the corresponding x,y,z coordinates into the field array, and field basearray index i?
+        // TODO: Sample 2x2x2 volume?
+        uint64_t x = floor(X*dx), y = floor(Y*dy), z = floor(Z*dz);
+        uint64_t i = z*ny*nx + y*nx + x;
 
-    if((voxels[I]>=1) && (field[i]>0)){ // Mask zeros in both voxels and field (TODO: should field be masked, or 0 allowed?)
-      int64_t field_index = floor(static_cast<double>(field_bins-1) * ((field[i] - fmin)/(fmax - fmin)) );
+        if((voxels[I]>=1) && (field[i]>0)){ // Mask zeros in both voxels and field (TODO: should field be masked, or 0 allowed?)
+            int64_t field_index = floor(static_cast<double>(field_bins-1) * ((field[i] - fmin)/(fmax - fmin)) );
 
-      bins[field_index*voxel_bins + voxel_index]++;
+            bins[field_index*voxel_bins + voxel_index]++;
+        }
     }
-  }
 }
 
 PYBIND11_MODULE(histograms, m) {
@@ -497,7 +495,8 @@ PYBIND11_MODULE(histograms, m) {
     m.def("axis_histogram_seq_cpu",  &axis_histogram_seq_cpu);
     m.def("axis_histogram_par_cpu",  &axis_histogram_par_cpu);
     m.def("axis_histogram_par_gpu",  &axis_histogram_par_gpu);
-    m.def("field_histogram", &field_histogram);
+    m.def("field_histogram_seq_cpu", &field_histogram_seq_cpu);
+    m.def("field_histogram_par_cpu", &field_histogram_par_cpu);
     m.def("masked_minmax", &masked_minmax);
     m.def("float_minmax", &float_minmax);    
 }
