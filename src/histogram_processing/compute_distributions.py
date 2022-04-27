775e94783991a214cc51862fc29e5dfb89fc3d14
James Avery <avery@diku.dk>
2022-04-27 15:26:33 +0200

    Start on distribution computation

---
+++ src/histogram_processing/compute_distributions.py
@@ -0,0 +1,10 @@
+import numpy as np, matplotlib.pyplot as plt, numpy.linalg as la
+from piecewise_cubic import piecewisecubic_matrix, piecewisecubic
+from config.paths import hdf5_root
+
+hist_path = f"{hdf5_root}/processed/histograms/"
+sample    = "770c"
+
+
+
+
