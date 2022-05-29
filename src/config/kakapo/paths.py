230c1fc80407b90f0b53f824c21e3c428227d0d6
James Avery <avery@diku.dk>
2022-05-29 07:01:10 +0200

    Laptop setup

---
+++ src/config/kakapo/paths.py
@@ -0,0 +1,44 @@
+hdf5_root      = "/home/avery/work/maxibone/data/"
+hdf5_root_fast = hdf5_root
+binary_root    = f"{hdf5_root_fast}/binary"
+
+esrf_data_local= f"{hdf5_root}/ESRF/"
+esrf_data_sftp =  "/XNS/XrayImaging/MiG/manjula.esci.nbi.dk.2_localhost/"
+esrf_implants_root  = f"{esrf_data_local}/esrf_dental_implants_april_2013/"
+esrf_granules_root  = f"{esrf_data_local}/esrf_dental_granules_july_2012/"
+hdf5_root      = ""
+esrf_implants_root  = "/data/MAXIBONE/manjula.esci.nbi.dk.2_localhost/esrf_dental_implants_april_2013/"
+esrf_granules_root  = "/data/MAXIBONE/manjula.esci.nbi.dk.2_localhost/esrf_dental_granules_july_2012/"
+
+
+# TODO: Hvorhen skal det her hen?
+import sys
+def commandline_args(defaults):
+    keys = list(defaults.keys())
+
+    helpstring = f"Syntax: {sys.argv[0]} "
+    for k in keys:
+        if(defaults[k] == "<required>"): helpstring += f" <{k}>"
+        else:                            helpstring += f" [{k}:{defaults[k]}]"
+
+    # Do we just want to know how to call this script?
+    if(len(sys.argv)==2):
+        if(sys.argv[1] == "--help" or sys.argv[1] == "-h"):
+            print(helpstring, file=sys.stderr)
+            sys.exit(1)
+
+    # Fill in parameters from commandline and defaults, converting to appropriate types
+    args = []
+    for i in range(len(keys)):
+        default = defaults[keys[i]]
+        if(len(sys.argv)<=i+1):
+            if(default == "<required>"):
+                print(helpstring, file=sys.stderr)
+                sys.exit(1)
+            else:
+                args.append(default)
+        else:                
+            args.append(type(default)(sys.argv[i+1]))
+
+    return args
+
