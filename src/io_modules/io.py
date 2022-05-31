a31a9d28821ec1c8c5366c4221c1a1f5dbaec1a3
James Avery <avery@nbi.dk>
2022-05-31 14:11:07 +0200

    Fixed 8 million bugs in update_hdf5

--- src/io_modules/io.py
+++ src/io_modules/io.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
+import h5py
 
-def update_hdf5(filename,group,datasets,attributes,compression="lzf",chunksize=(64,64,64)):
+def update_hdf5(filename,group_name,datasets,attributes,compression="lzf",chunksize=(64,64,64)):
     f = h5py.File(filename,'a')
 
-    g = f.require_group(f)
+    g = f.require_group(group_name)
     
-    for k,v in datasets:
-        g.require_dataset(k,shape=v.shape,dtype=v.dtype,compression=compression, chunksize=chunksize)
+    for k in datasets:
+        v = datasets[k]
+        g.require_dataset(k,shape=v.shape,dtype=v.dtype,compression=compression, chunks=chunksize)
         g[k][:] = v[:]
 
-    for k,v in attributes:
-        g.attr(k) = v
+    for k in attributes:
+        v = attributes[k]
+        g.attrs[k] = v
 
     f.close()
