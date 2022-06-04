dd9388a3734211c2a8f9b4c97a28c7ae016cd1df
James Avery <avery@nbi.dk>
2022-06-04 23:17:09 +0200

    Use HDF5 groups instead of one-file-per-subvolume and {name}_prop1, {name}_prop2,...

--- src/scripts/generate_otsu_probabilities.py
+++ src/scripts/generate_otsu_probabilities.py
@@ -1,15 +1,12 @@
-import sys
+import os,sys, pathlib, h5py, numpy as np
 sys.path.append(sys.path[0]+"/../")
-import cv2
-import h5py
 import pybind_kernels.histograms as histograms
-import numpy as np
 from config.paths import binary_root, hdf5_root_fast as hdf5_root, commandline_args
 from tqdm import tqdm
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt, matplotlib.cm as cm
 from histogram_processing.piecewise_cubic import piecewisecubic, piecewisecubic_matrix, smooth_fun
 from PIL import Image
-import os
+from numpy import newaxis as NA
 import helper_functions
 
 def apply_otsu(bins, name=None):
@@ -56,7 +53,7 @@ def apply_otsu(bins, name=None):
 
         # Plot the thresholds on top of the original image
         display_cubic = ((bins / bins.max()) * 255).astype(np.uint8)
-        display_cubic = cv2.cvtColor(display_cubic, cv2.COLOR_GRAY2RGB)
+        display_cubic = np.broadcast_to(display_cubic[:,:,NA],display_cubic.shape+(3,))
         for i, thresh in enumerate(threshes):
             display_cubic[i,int(thresh)-2:int(thresh)+2] = (255,128,0) # otsu is orange
             display_cubic[i,int(new_threshes_cubic[i])-2:int(new_threshes_cubic[i])+2] = (255,0,0) # cubic is red
@@ -74,41 +71,44 @@ def extract_probabilities(labeled, axes_names, field_names):
         Ps.append(apply_otsu(bins, f'field_bins_{name}'))
     return Ps
 
-def save_probabilities(Ps, output_path, value_ranges):
-    group_name = 'otsu_seperation'
+def save_probabilities(Ps, sample, subbins,value_ranges):
+    output_path = f'{hdf5_root}/processed/probabilities/{sample}.h5'
     helper_functions.update_hdf5(
         output_path,
-        group_name = group_name,
+        group_name = f'otsu_seperation/{subbins}',
         datasets = { 'value_ranges' : value_ranges },
         attributes = {}
     )
     for name, P0, P1, pc, valid_range, threshes, new_threshes in Ps:
         helper_functions.update_hdf5(
             output_path,
-            group_name = group_name,
+            group_name = f'otsu_seperation/{subbins}/{name}',
             datasets = {
-                f'{name}_c0': P0,
-                f'{name}_c1': P1,
-                f'{name}_pc_coefs': pc[0],
-                f'{name}_pc_borders': pc[1],
-                f'{name}_threshes': threshes,
-                f'{name}_new_threshes': new_threshes
+                f'c0': P0,
+                f'c1': P1,
+                f'pc_coefs':   pc[0],
+                f'pc_borders': pc[1],
+                f'threshes': threshes,
+                f'new_threshes': new_threshes
             },
             attributes = {
-                f'{name}_valid_start': valid_range[0],
-                f'{name}_valid_end': valid_range[1],
+                f'valid_start': valid_range[0],
+                f'valid_end': valid_range[1],
             }
         )
 
 if __name__ == '__main__':
     sample, subbins, debug_output = commandline_args({'sample':'<required>', 'subbins': '<required>', 'debug_output': None})
     output_folder = f'{hdf5_root}/processed/probabilities/'
-    debug = True
+    debug = False
     debug_output = output_folder if not debug_output else debug_output
-    if not os.path.exists(debug_output):
-        os.mkdir(debug_output)
-    bins = np.load(f'{hdf5_root}/processed/histograms/{sample}/bins-{subbins}.npz')
+
+
+    pathlib.Path(debug_output).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(output_folder).mkdir(parents=True, exist_ok=True)
+
+    bins = np.load(f'{hdf5_root}/processed/histograms/{sample}/bins-{subbins}.npz') # TODO: Switch to h5
     axes_names = [name.split('_bins')[0] for name in bins.keys() if '_bins' in name and 'field' not in name]
     field_names = bins['field_names']
     Ps = extract_probabilities(bins, axes_names, field_names)
-    save_probabilities(Ps, f'{hdf5_root}/processed/probabilities/{sample}-{subbins}.h5', bins['value_ranges'])
+    save_probabilities(Ps, sample, subbins, bins['value_ranges'])
