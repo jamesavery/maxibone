11bc10eca3f84c04ff9c1d8d6c17570404c598a6
James Avery <avery@nbi.dk>
2022-06-05 17:16:26 +0200

    Get labeling for one field to work.

--- src/pybind_kernels/label.cc
+++ src/pybind_kernels/label.cc
@@ -77,7 +77,7 @@ void material_prob(const py::array_t<voxel_type> &np_voxels,
 
     // TODO this might fail if the first of each is not enabled by the mask.
     const uint32_t
-        Nvoxel_bins = n_axes > 0 ? axes_probs_info[0].shape[1] : 0,
+        Nvoxel_bins = n_axes > 0 ? axes_probs_info[0].shape[1] : 0, // TODO: Don't depend on axes being used
         Nfield_bins = n_fields > 0 ? field_probs_info[0].shape[2] : 0;
 
     const voxel_type *voxels = static_cast<const voxel_type*>(voxels_info.ptr);
@@ -87,23 +87,31 @@ void material_prob(const py::array_t<voxel_type> &np_voxels,
 
     auto [sz, sy, sx] = offset;
     auto [Nz, Ny, Nx] = ranges;
+    
     uint64_t
-        fz = (Nz-sz) / 2, //TODO this is off
+        fz = (Nz-sz) / 2, //TODO this is off // TODO: How so?
         fy = Ny / 2,
         fx = Nx / 2;
     auto [v_min, v_max] = vrange;
     auto [f_min, f_max] = frange;
 
-    #pragma omp parallel for collapse(3)
+    fprintf(stderr,"(Nz,Ny,Nx) = (%d,%d,%d)\n",Nz,Ny,Nx);
+    fprintf(stderr,"(sz,sy,sx) = (%d,%d,%d)\n",sz,sy,sx);
+    fprintf(stderr,"n_axes = %ld, n_fields = %ld\n",n_axes,n_fields);
+    
+
     for (uint64_t z = sz; z < Nz; z++) {
+      fprintf(stderr,"z = %ld\n",z);
+#pragma omp parallel for collapse(2)      
         for (uint64_t y = sy; y < Ny; y++) {
             for (uint64_t x = sx; x < Nx; x++) {
                 // TODO Only compute the indices and such if they're actually used.
-                uint64_t flat_index = (z-sz)*Ny*Nx + y*Nx + x;
-                // Get the voxel value and the indices
+                uint64_t flat_index = (z-sz)*Ny*Nx + y*Nx + x; // TODO: Shouldn't y and x be offset, too??
+                // get the voxel value and the indices
                 voxel_type voxel = voxels[flat_index];
                 if (voxel < v_min || voxel > v_max)
                     continue;
+		
                 int64_t voxel_index = floor(static_cast<double>(Nvoxel_bins-1) * ((voxel - v_min)/(v_max - v_min)) );
                 uint64_t r = floor(sqrt((x-Nx/2.0)*(x-Nx/2.0) + (y-Ny/2.0)*(y-Ny/2.0)));
 
@@ -129,8 +137,100 @@ void material_prob(const py::array_t<voxel_type> &np_voxels,
     }
 }
 
+void material_prob_justonefieldthx(const py::array_t<voxel_type> &np_voxels,
+				   const py::array_t<field_type> &np_field,
+				   const py::array_t<prob_type>  &np_prob,			      
+				   py::array_t<result_type> &np_result,
+				   const std::tuple<float, float> &vrange,
+				   const std::tuple<float, float> &frange,
+				   const std::tuple<uint64_t, uint64_t, uint64_t> &offset,
+				   const std::tuple<uint64_t, uint64_t, uint64_t> &ranges) {
+
+    py::buffer_info
+      voxels_info = np_voxels.request(),
+      field_info  = np_field.request(),
+      prob_info   = np_prob.request(),
+      result_info = np_result.request();
+
+    const voxel_type *voxels = static_cast<const voxel_type*>(voxels_info.ptr);
+    const field_type *field  = static_cast<const field_type*>(field_info.ptr);
+    const prob_type  *prob   = static_cast<prob_type*>(prob_info.ptr);
+    result_type      *result = static_cast<result_type*>(result_info.ptr);
+
+    const uint32_t Nfield_bins = prob_info.shape[0],
+                   Nvoxel_bins = prob_info.shape[1];
+    
+    auto [sz, sy, sx] = offset;
+    auto [Nz, Ny, Nx] = ranges;
+    const auto nz = voxels_info.shape[0], ny = voxels_info.shape[1], nx = voxels_info.shape[2];
+    const auto fz = field_info.shape[0],  fy = field_info.shape[1],  fx = field_info.shape[2];
+
+    auto [v_min, v_max] = vrange;
+    auto [f_min, f_max] = frange;
+
+    fprintf(stderr,"(Nz,Ny,Nx) = (%d,%d,%d)\n",Nz,Ny,Nx);
+    fprintf(stderr,"(sz,sy,sx) = (%d,%d,%d)\n",sz,sy,sx);
+    fprintf(stderr,"(nz,ny,nx) = (%d,%d,%d)\n",nz,ny,nx);
+    fprintf(stderr,"(fz,fy,fx) = (%d,%d,%d)\n",fz,fy,fx);
+    fprintf(stderr,"(v_min,v_max) = (%g,%g)\n",v_min,v_max);
+    fprintf(stderr,"(f_min,f_max) = (%g,%g)\n",f_min,f_max);
+
+    {
+      prob_type p_min = 0, p_max = 0;
+      for(size_t i=0;i<prob_info.size;i++){ p_min = std::min(p_min,prob[i]); p_max = std::max(p_max,prob[i]); }
+      fprintf(stderr,"prob min,max = %g,%g\n",p_min,p_max);
+      
+      
+      field_type field_min = 0, field_max = 0;
+      for(size_t i=0;i<field_info.size;i++){
+	field_min = std::min(field_min,field[i]); field_max = std::max(field_max,field[i]);
+      }
+      fprintf(stderr,"field min,max = %d,%d\n",field_min,field_max);
+
+      voxel_type vox_min = 0, vox_max = 0;
+      for(size_t i=0;i<voxels_info.size;i++){
+	vox_min = std::min(vox_min,voxels[i]); vox_max = std::max(vox_max,voxels[i]);
+      }
+      fprintf(stderr,"voxels min,max = %d,%d\n",vox_min,vox_max);
+    }
+    
+#pragma omp parallel for collapse(3)          
+    for (uint64_t z = sz; z < Nz; z++) {
+        for (uint64_t y = sy; y < Ny; y++) {
+            for (uint64_t x = sx; x < Nx; x++) {
+                uint64_t flat_index = (z-sz)*Ny*Nx + y*Nx + x;
+
+                // get the voxel value and the indices
+                voxel_type voxel = voxels[flat_index];
+                if (voxel < v_min || voxel > v_max)
+                    continue;
+		
+                int64_t voxel_index = floor(static_cast<double>(Nvoxel_bins-1) * ((voxel - v_min)/(v_max - v_min)) );
+
+                prob_type p = 0;
+
+		// TODO: Allow variable voxels scale and field scale (dx = Nx/float(fx), etc.)
+		uint64_t flat_field_index = ((z-sz)/2)*fy*fx + (y/2)*fx + (x/2);
+		field_type field_v        = field[flat_field_index];
+
+		if (field_v < f_min || field_v > f_max)
+                        continue;
+		int64_t field_index = floor(static_cast<double>(Nfield_bins-1) * ((field_v - f_min)/(f_max - f_min)) );
+		p += prob[field_index*Nfield_bins + voxel_index]; //* weights[i + n_axes];
+
+                // Compute the joint probability and cast between [0:result_type_max_value]
+                result[flat_index] = round(p * std::numeric_limits<result_type>::max());
+            }
+        }
+    }
+    result_type r_min = 0, r_max = 0;
+    for(size_t i=0;i<result_info.size;i++){ r_min = std::min(r_min,result[i]); r_max = std::max(r_max,result[i]); }
+    fprintf(stderr,"result min,max = %g,%g\n",r_min,r_max);
+}
+
 PYBIND11_MODULE(label, m) {
     // optional module docstring
     m.doc() = "Mapping material probability distributions to a tomography.";
     m.def("material_prob", &material_prob);
+    m.def("material_prob_justonefieldthx",&material_prob_justonefieldthx);
 }
--- src/scripts/segment_from_distributions.py
+++ src/scripts/segment_from_distributions.py
@@ -1,34 +1,20 @@
-import sys
+import os, sys, pathlib, h5py, numpy as np
 sys.path.append(sys.path[0]+"/../")
-import cv2
-import h5py
 import pybind_kernels.histograms as histograms
 import pybind_kernels.label as label
-import numpy as np
 from config.paths import binary_root, hdf5_root_fast as hdf5_root, commandline_args
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from PIL import Image
-from helper_functions import block_info
+from helper_functions import block_info, load_block
 
 debug = True
 
-def load_block(sample, Ny, Nx, z_offset, block_size, field_names):
-    Nfields = len(field_names)
-    voxels = np.zeros((block_size,Ny,Nx),    dtype=np.uint16)
-    fields = np.zeros((Nfields,block_size//2,Ny//2,Nx//2), dtype=np.uint16)
-    histograms.load_slice(voxels, f'{binary_root}/voxels/1x/{sample}.uint16', (z_offset, 0, 0), (block_size, Ny, Nx))
-    for i in range(Nfields):
-        fi = np.load(f"{binary_root}/fields/implant-{field_names[i]}/2x/{sample}.npy", mmap_mode='r')
-        fields[i,:] = fi[z_offset//2:z_offset//2 + block_size//2,:Ny//2,:Nx//2]
-
-    return voxels, fields
 
 def load_probabilities(path, group, axes_names, field_names, c):
-    with h5py.File(path, 'r') as f:
-        grp = f[group]
-        P_axes = [grp[f'{name}_bins_c{c}'][:,:] for name in axes_names]
-        P_fields = [grp[f'field_bins_{name}/c{c}'][:,:] for name in field_names]
+    with h5py.File(path, 'r') as prob_file:
+        P_axes   = [prob_file[f'{group}/{name}/c{c}'][:,:] for name in axes_names]
+        P_fields = [prob_file[f'{group}/{name}/c{c}'][:,:] for name in field_names]
     return P_axes, P_fields
 
 def load_value_ranges(path, group):
@@ -42,18 +28,23 @@ def nblocks(size, block_size):
     return (size // block_size) + (1 if size % block_size > 0 else 0)
 
 if __name__ == '__main__': 
-    sample, subbins, group, debug_output = commandline_args({'sample':'<required>', 'subbins': '<required>', 'group': 'otsu_separation', 'debug_output': None})
+    sample, block_start, n_blocks, region, group, debug_output = commandline_args({'sample':'<required>',
+                                                                                   "block_start":0,
+                                                                                   "n_blocks":0,
+                                                                                   'region': 'bone_region',
+                                                                                   'group': 'otsu_separation',
+                                                                                   'debug_output': None})
 
     # Iterate over all subvolumes
-    bi = block_info(f'{hdf5_root}/hdf5-byte/msb/{sample}.h5', block_size=0, n_blocks=0, z_offset=0)
+    bi = block_info(f'{hdf5_root}/hdf5-byte/msb/{sample}.h5', block_size=0, n_blocks=n_blocks, z_offset=block_start)
     sz, sy, sx = bi['dimensions'][:3]
     fz, fy, fx = np.array((sz, sy, sx)) // 2
     axes_names =  []     # ["x", "y", "z", "r"] # For later
     field_names = ["edt"]# ["gauss", "edt", "gauss+edt"] 
 
     probs_file = f'{hdf5_root}/processed/probabilities/{sample}.h5'    
-    for b in tqdm(range(bi['n_blocks']), desc='segmenting subvolumes'):
-        group_name = f"{group}/{subbins}{b}/"
+    for b in tqdm(range(block_start,block_start+bi['n_blocks']), desc='segmenting subvolumes'):
+        group_name = f"{group}/{region}{b}/"
         block_size = bi['subvolume_nzs'][b]
         zstart = bi['subvolume_starts'][b]
         zend = zstart + block_size
@@ -64,22 +55,28 @@ if __name__ == '__main__':
         vranges = np.array([vmin, vmax, fmin, fmax], np.float32)
 
         for c in [0,1]:
-            output_file = f'{hdf5_root}/processed/segmented/{sample}_{c}.bin'
+            output_dir = f'{binary_root}/segmented/1x/'
+            pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+
             P_axes, P_fields = load_probabilities(probs_file, group_name, axes_names, field_names, c)
             n_probs = len(P_axes) + len(P_fields)
             result = np.zeros((bi['subvolume_nzs'][b],sy,sx), dtype=np.uint16)
 
-            label.material_prob(
-                voxels, fields,
-                P_axes, 0b1111,
-                P_fields, 0b111,
-                np.array([1. / n_probs] * n_probs), # Average of all of the probabilities
-                result,
-                (vmin, vmax), (fmin, fmax),
-                (zstart, 0, 0), (zend, sy, sx)
-            )
+            label.material_prob_justonefieldthx(voxels,fields[0],P_fields[0],result,
+                                                (vmin,vmax),(fmin,fmax),
+                                                (zstart,0,0), (zend,0,0));
+            # label.material_prob(
+            #     voxels, fields,
+            #     P_axes, 0,#0b1111,
+            #     P_fields, 0b01,#0b111,
+            #     np.array([1. / n_probs] * n_probs), # Average of all of the probabilities
+            #     result,
+            #     (vmin, vmax), (fmin, fmax),
+            #     (zstart, 0, 0), (zend, sy, sx)
+            # )
 
             if debug:
                 print (f'Segmentation has min {result.min()} and max {result.max()}')
 
-            histograms.write_slice(result, zstart, output_file)
+            
+#            histograms.write_slice(result, zstart, output_file)
