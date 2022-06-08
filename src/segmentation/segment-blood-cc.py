33246f2e0a311476a8e1d35340b0392807c77146
James Avery <avery@nbi.dk>
2022-06-08 19:27:43 +0200

    Two bugfixes in segment-blood-cc.py
     - Probabilities were not thresholded before computing largest component, leading to massive amounts of other stuff being classified as blood vessels.
     - Dimensions were wrong (not volume-matching corrected).

--- src/segmentation/segment-blood-cc.py
+++ src/segmentation/segment-blood-cc.py
@@ -6,31 +6,39 @@ from pybind_kernels.geometry import center_of_mass, inertia_matrix, integrate_ax
 from pybind_kernels.histograms import load_slice
 from io_modules.io import update_hdf5
 from scipy import ndimage as ndi
+from helper_functions import *
 
 sample, c, chunk_size = commandline_args({"sample":"<required>", "material":0, "chunk_size":256})
 
 scales = [32, 16, 8, 4, 2]
 
-meta = f'{hdf5_root}/hdf5-byte/msb/{sample}.h5'
-h5meta = h5py.File(meta, 'r')
-Nz, Ny, Nx = h5meta['voxels'].shape
+bi = block_info(f'{hdf5_root}/hdf5-byte/msb/{sample}.h5')
+Nz, Ny, Nx, _ = bi["dimensions"]
 
 for scale in tqdm.tqdm(scales, desc= 'Computing connected components'):
     data = f'{binary_root}/segmented/c{c}/{scale}x/{sample}.uint16'
     output_dir = f'{hdf5_root_fast}/masks/{scale}x'
     pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
     nz, ny, nx = Nz // scale, Ny // scale, Nx // scale
-    buffer = np.empty((nz, ny, nx), dtype=np.uint16)
-    load_slice(buffer, data, (0,0,0), (nz, ny, nx))
-    label, n_features = ndi.label(buffer)
+    voxel_size = bi["voxel_size"]*scale
+    
+    voxels = np.empty((nz, ny, nx), dtype=np.uint16)    
+    load_slice(voxels, data, (0,0,0), (nz, ny, nx))
+    
+#    hist,bins = np.histogram(voxels.flatten(), 256)
+#    plt.plot(bins[11:],hist[10:]); plt.show()
+
+    mask = voxels > 18000
+#    mask = voxels > 40000    
+    
+    label, n_features = ndi.label(mask)
     counts = np.bincount(label[label>0], minlength=n_features+1)
     largest_cc = np.argmax(counts)
     mask = (label == largest_cc)
-    voxel_size = h5meta['voxels'].attrs['voxelsize'] * scale
+
     update_hdf5(f"{output_dir}/{sample}.h5",
-                group_name=f"c{c}",
+                group_name=f"blood",
                 datasets={'mask':mask},
                 attributes={'scale':scale,'voxel_size':voxel_size,
-                            'sample':sample, 'name':"implant_mask"})
+                            'sample':sample, 'name':"blood_mask"})
 
-h5meta.close()
