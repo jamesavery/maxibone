499f1a7b3d66d7e7676e2fd2ff85303405286f62
James Avery <avery@nbi.dk>
2022-06-22 15:02:49 +0200

    Update segment-blood-cc

--- src/segmentation/segment-blood-cc.py
+++ src/segmentation/segment-blood-cc.py
@@ -2,13 +2,11 @@ import h5py, sys, os.path, pathlib, numpy as np, scipy.ndimage as ndi, tqdm, mat
 sys.path.append(sys.path[0]+"/../")
 from config.constants import *
 from config.paths import hdf5_root, hdf5_root_fast, binary_root, commandline_args
-from pybind_kernels.geometry import center_of_mass, inertia_matrix, integrate_axes, sample_plane
 from pybind_kernels.histograms import load_slice
-from io_modules.io import update_hdf5
 from scipy import ndimage as ndi
 from helper_functions import *
 
-sample, c, chunk_size = commandline_args({"sample":"<required>", "material":0, "chunk_size":256})
+sample, m, chunk_size = commandline_args({"sample":"<required>", "material":0, "chunk_size":256})
 
 scales = [32, 16, 8, 4, 2]
 
@@ -16,7 +14,7 @@ bi = block_info(f'{hdf5_root}/hdf5-byte/msb/{sample}.h5')
 Nz, Ny, Nx, _ = bi["dimensions"]
 
 for scale in tqdm.tqdm(scales, desc= 'Computing connected components'):
-    data = f'{binary_root}/segmented/c{c}/{scale}x/{sample}.uint16'
+    data = f'{binary_root}/segmented/P{m}/{scale}x/{sample}.uint16'
     output_dir = f'{hdf5_root_fast}/masks/{scale}x'
     pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
     nz, ny, nx = Nz // scale, Ny // scale, Nx // scale
@@ -28,8 +26,8 @@ for scale in tqdm.tqdm(scales, desc= 'Computing connected components'):
 #    hist,bins = np.histogram(voxels.flatten(), 256)
 #    plt.plot(bins[11:],hist[10:]); plt.show()
 
-    mask = voxels > 18000
-#    mask = voxels > 40000    
+#    mask = voxels > 32000
+    mask = voxels > 40000    
     
     label, n_features = ndi.label(mask)
     counts = np.bincount(label[label>0], minlength=n_features+1)
