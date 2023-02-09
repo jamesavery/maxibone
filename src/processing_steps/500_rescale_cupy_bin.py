bf63dc0030dc037547c9884272f0dd56ead61eba
Carl Johnsen <carl-johannes@di.ku.dk>
2023-02-09 17:47:30 +0100

    #29 added verbose to rescale_cupy_bin

--- src/processing_steps/500_rescale_cupy_bin.py
+++ src/processing_steps/500_rescale_cupy_bin.py
@@ -12,10 +12,12 @@ pinned_mempool = cp.get_default_pinned_memory_pool()
 mempool.free_all_blocks()
 pinned_mempool.free_all_blocks()
 
-
 if __name__ == "__main__":
-    sample, image, chunk_size, dtype = commandline_args({"sample":"<required>", "image": "voxels",
-                                                         "chunk_size":32*2, "dtype":"uint16"})
+    sample, image, chunk_size, dtype, verbose = commandline_args({"sample" : "<required>", 
+                                                                  "image" :  "voxels",
+                                                                  "chunk_size" : 32*2, 
+                                                                  "dtype" : "uint16",
+                                                                  "verbose" : 1})
 
     scales = [2,4,8,16,32];     # Can do 6, 9, 12, 24, 27, etc. as well, but we currently don't. See old rescaly-cupy.py
     T = np.dtype(dtype)
@@ -23,10 +25,10 @@ if __name__ == "__main__":
     input_meta  = f'{hdf5_root}/hdf5-byte/msb/{sample}.h5'
     input_bin   = f"{binary_root}/{image}/1x/{sample}.{dtype}"
     output_root = f"{binary_root}/{image}"
-    print(f"Generating power-of-twos rescalings for sample {sample}")
-    print(f"Input metadata from {input_meta}")
-    print(f"Input flat binary {dtype} data from {input_bin}")
-    print(f"Output flat binary {dtype} data to {output_root}/[1,2,4,8,16,32]x/{sample}.{dtype}")
+    if verbose >= 1: print(f"Generating power-of-twos rescalings for sample {sample}")
+    if verbose >= 1: print(f"Input metadata from {input_meta}")
+    if verbose >= 1: print(f"Input flat binary {dtype} data from {input_bin}")
+    if verbose >= 1: print(f"Output flat binary {dtype} data to {output_root}/[1,2,4,8,16,32]x/{sample}.{dtype}")
     
     meta_h5    = h5py.File(input_meta, 'r')
     full_Nz, Ny, Nx = meta_h5['voxels'].shape
@@ -34,13 +36,13 @@ if __name__ == "__main__":
     Nz         = full_Nz - np.sum(shifts)
     meta_h5.close()    
     
-    print(f"Downscaling from 1x {(Nz,Ny,Nx)} to 2x {(Nz//2,Ny//2,Nx//2)}")
+    if verbose >= 1: print(f"Downscaling from 1x {(Nz,Ny,Nx)} to 2x {(Nz//2,Ny//2,Nx//2)}")
     if(chunk_size % 32):
-        print(f"Chunk size {chunk_size} is invalid: must be divisible by 32.")
+        if verbose >= 1: print(f"Chunk size {chunk_size} is invalid: must be divisible by 32.")
         sys.exit(-1)
-#        print(f"Used GPU memory: {mempool.used_bytes()//1000000}MB out of {mempool.total_bytes()/1000000}MB. {pinned_mempool.n_free_blocks()} free pinned blocks.")
+#        if verbose >= 1: print(f"Used GPU memory: {mempool.used_bytes()//1000000}MB out of {mempool.total_bytes()/1000000}MB. {pinned_mempool.n_free_blocks()} free pinned blocks.")
 
-#    print(f"Used GPU memory: {mempool.used_bytes()//1000000}MB out of {mempool.total_bytes()/1000000}MB. {pinned_mempool.n_free_blocks()} free pinned blocks.")
+#    if verbose >= 1: print(f"Used GPU memory: {mempool.used_bytes()//1000000}MB out of {mempool.total_bytes()/1000000}MB. {pinned_mempool.n_free_blocks()} free pinned blocks.")
 
     # TODO: Just iterate now we do powers of two
     voxels2x  = np.empty((Nz//2,Ny//2,Nx//2),dtype=T)
@@ -61,10 +63,10 @@ if __name__ == "__main__":
         try:
             voxels1x_chunk = cp.fromfile(input_bin, dtype=T, count=chunk_items, offset=z*Ny*Nx*T.itemsize).reshape(zend-z,Ny,Nx)
         except:
-            print(f"Read failed. chunk_items = {chunk_items} = {(zend-z)*Ny*Nx}, z = {z}, zend-z = {zend-z}")
+            if verbose >= 1: print(f"Read failed. chunk_items = {chunk_items} = {(zend-z)*Ny*Nx}, z = {z}, zend-z = {zend-z}")
             sys.exit(-1)
             
-#        print(f"Used GPU memory: {mempool.used_bytes()//1000000}MB out of {mempool.total_bytes()/1000000}MB. {pinned_mempool.n_free_blocks()} free pinned blocks.")
+#        if verbose >= 1: print(f"Used GPU memory: {mempool.used_bytes()//1000000}MB out of {mempool.total_bytes()/1000000}MB. {pinned_mempool.n_free_blocks()} free pinned blocks.")
         voxels2x_chunk = downsample2x(voxels1x_chunk)
         del voxels1x_chunk
         voxels4x_chunk  = downsample2x(voxels2x_chunk)
@@ -90,10 +92,10 @@ if __name__ == "__main__":
         del voxels16x_chunk
         del voxels32x_chunk
         
-    print(f"Allocating {(Nz//2,Ny//2,Nx//2)}={Nz//2*Ny//2*Nx//2} {dtype} for voxels2x on GPU")
+    if verbose >= 1: print(f"Allocating {(Nz//2,Ny//2,Nx//2)}={Nz//2*Ny//2*Nx//2} {dtype} for voxels2x on GPU")
     
     for i in tqdm.tqdm(range(len(scales)),f"{sample}: Downscaling to all smaller scales: {scales[2:]}"):
         output_dir = f"{output_root}/{scales[i]}x/"
         pathlib.Path(f"{output_dir}").mkdir(parents=True, exist_ok=True)            
-        print(f"Writing out scale {scales[i]}x {(voxels[i].shape)} to {output_dir}/{sample}.uint16")
+        if verbose >= 1: print(f"Writing out scale {scales[i]}x {(voxels[i].shape)} to {output_dir}/{sample}.uint16")
         voxels[i].tofile(f"{output_dir}/{sample}.uint16")
