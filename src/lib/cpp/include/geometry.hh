ec9b2f62b2ad992117332642805226dd5c9e4d1d
Carl Johnsen <carl-johannes@di.ku.dk>
2023-02-15 11:53:36 +0100

    #34 Added example docstring in geometry.hh

--- src/lib/cpp/include/geometry.hh
+++ src/lib/cpp/include/geometry.hh
@@ -15,7 +15,21 @@ void print_timestamp(string message) {
     //fprintf(stderr,"%s at %02d:%02d:%02d\n", message.c_str(), local_tm.tm_hour, local_tm.tm_min, local_tm.tm_sec);
 }
 
+/*
+Computes the center of mass of the given tomography.
+
+@param voxels The given tomography.
+@returns The 3D coordinates of the center of mass (in Z, Y, X).
+*/
 array<real_t,3> center_of_mass(const input_ndarray<mask_type> voxels);
+
+/*
+Computes the inertia matrix of the given tomography based of the given center of mass.
+
+@param voxels The given tomography.
+@param cm The given center of mass.
+@returns The 3x3 inertia matrix.
+*/
 array<real_t,9> inertia_matrix(const input_ndarray<mask_type> &voxels, const array<real_t,3> &cm);
 
 #endif
