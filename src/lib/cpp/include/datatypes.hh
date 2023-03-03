3c81ebb497fd62e318cbcbc354fecc63a1f9ef95
Carl Johnsen <carl-johannes@di.ku.dk>
2023-03-03 16:07:41 +0100

    #25 Handled shadow warning for ndarray struct

--- src/lib/cpp/include/datatypes.hh
+++ src/lib/cpp/include/datatypes.hh
@@ -19,8 +19,8 @@ template <typename voxel_type>
 using np_array = py::array_t<voxel_type, py::array::c_style | py::array::forcecast>;
 
 typedef py::array_t<mask_type, py::array::c_style | py::array::forcecast> np_maskarray;
-typedef py::array_t<real_t, py::array::c_style | py::array::forcecast>    np_realarray;
-typedef py::array_t<uint8_t, py::array::c_style | py::array::forcecast>   np_bytearray;
+typedef py::array_t<real_t,    py::array::c_style | py::array::forcecast> np_realarray;
+typedef py::array_t<uint8_t,   py::array::c_style | py::array::forcecast> np_bytearray;
 
 template <typename T>
 constexpr ssize_t acc_block_size = 1024 * 1024 * 1024 / sizeof(T); // 1 GB
@@ -33,16 +33,16 @@ template <typename T> struct input_ndarray {
   const T *data;
   const vector<ssize_t> shape;
 
-  input_ndarray(const T *data, const vector<ssize_t> &shape): data(data), shape(shape) {}
-  input_ndarray(const void *data, const vector<ssize_t> &shape): data(static_cast<const T*>(data)), shape(shape) {}
+  input_ndarray(const T *arg_data, const vector<ssize_t> &arg_shape): data(arg_data), shape(arg_shape) {}
+  input_ndarray(const void *arg_data, const vector<ssize_t> &arg_shape): data(static_cast<const T*>(arg_data)), shape(arg_shape) {}
 };
 
 template <typename T> struct output_ndarray {
   T *data;
   const vector<ssize_t> shape;
 
-  output_ndarray(T *data, const vector<ssize_t> &shape): data(data), shape(shape) {}
-  output_ndarray(void *data, const vector<ssize_t> &shape): data(static_cast<T*>(data)), shape(shape) {}
+  output_ndarray(T *arg_data, const vector<ssize_t> &arg_shape): data(arg_data), shape(arg_shape) {}
+  output_ndarray(void *arg_data, const vector<ssize_t> &arg_shape): data(static_cast<T*>(arg_data)), shape(arg_shape) {}
 };
 
 typedef std::array<real_t,16> matrix4x4;
