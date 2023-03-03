701614a7a4d3ddb30d938539782e2ca520d19027
Carl Johnsen <carl-johannes@di.ku.dk>
2023-03-03 15:05:05 +0100

    #25 Added additional C++ warnings. Removed the ones from python / pybind

--- src/pybind/geometry-pybind.cc
+++ src/pybind/geometry-pybind.cc
@@ -1,12 +1,12 @@
 #ifdef _OPENACC
-#warning "Using GPU"
+//#warning "Using GPU"
 #define NS gpu
 #else
 #ifdef _OPENMP
-#warning "Using OpenMP"
+//#warning "Using OpenMP"
 #define NS cpu_par
 #else
-#warning "Using sequential"
+//#warning "Using sequential"
 #define NS cpu_seq
 #endif
 #endif
