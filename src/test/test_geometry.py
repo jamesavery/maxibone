1405c48e4302cabbab27a3670e140828eda85933
Carl Johnsen <carl-johannes@di.ku.dk>
2023-03-03 13:53:09 +0100

    #25 Added functionality to the geometry test helper functions

--- src/test/test_geometry.py
+++ src/test/test_geometry.py
@@ -16,36 +16,52 @@ import pytest
 #n = 2344 # ~12 GB, used for testing whether blocked works.
 n = 128
 
-def assert_with_print(a, b):
-    all_close = np.allclose(a, b)
+def assert_with_print(a, b, tolerance=1e-7, names=None):
+    na, nb = np.array(a), np.array(b)
+    nabs = np.abs(na - nb)
+    all_close = np.alltrue(nabs < tolerance)
     if not all_close:
-        na, nb = np.array(a), np.array(b)
-        print (na)
-        print (nb)
-        nabs = np.abs(na - nb)
-        print (nabs)
-        print (np.sum(nabs))
+        print ('a', na)
+        print ('b', nb)
+        print ('absolute error (AE) (abs(a-b))', nabs)
+        print ('AE sum', np.sum(nabs))
+        diffs = np.argwhere(nabs > tolerance)
+        print (f'differing on {diffs.shape} elements')
+        for i in diffs[:5]: # Only print 5 first
+            print ('differing index (i), a[i], b[i] =', i, a[i[0], i[1]], b[i[0], i[1]])
+        if not names is None:
+            print (names)
     assert all_close
 
-def run_with_warmup(f):
-    f()
+def run_with_warmup(f, allocate_result=None):
+    '''
+    Runs the given function and returns the result and how long time it took to run.
+
+    @param allocate_result Defines whether the memory for the result should be allocated before running. If it should, it should be a tuple of the shape and the dtype of the array. None otherwise.
+    '''
+    alloc = lambda x: np.zeros(x[0], x[1])
+    f() if allocate_result is None else f(alloc(allocate_result))
+    result = alloc(allocate_result)
     start = datetime.datetime.now()
-    result = f()
+    if allocate_result is None:
+        result = f()
+    else:
+        f(result)
     end = datetime.datetime.now()
     return result, end - start
 
-def compare_fs(func, baseline_f, cpu_f, gpu_f, should_assert=True):
-    baseline, baseline_t = run_with_warmup(baseline_f)
+def compare_fs(func, baseline_f, cpu_f, gpu_f, should_assert=True, tolerance=1e-7,
+               allocate_result: tuple[tuple[int],np.dtype]=None):
+    baseline, baseline_t = run_with_warmup(baseline_f, allocate_result)
     print (f'({func}) Sequential ran in {baseline_t}')
 
-    cpu, cpu_t = run_with_warmup(cpu_f)
+    cpu, cpu_t = run_with_warmup(cpu_f, allocate_result)
     print (f'({func}) Parallel CPU ran in {cpu_t}, which is {baseline_t / cpu_t} times faster than sequential')
-    if should_assert: assert_with_print(baseline, cpu)
+    if should_assert: assert_with_print(baseline, cpu, tolerance, 'cpu_seq vs cpu')
 
-    gpu, gpu_t = run_with_warmup(gpu_f)
+    gpu, gpu_t = run_with_warmup(gpu_f, allocate_result)
     print (f'({func}) GPU ran in {gpu_t}, which is {baseline_t / gpu_t} times faster than sequential')
-    if should_assert: assert_with_print(baseline, gpu)
-
+    if should_assert: assert_with_print(baseline, gpu, tolerance, 'cpu_seq vs gpu')
 
 def test_center_of_mass():
     voxels = np.random.randint(0, 256, (n,n,n), np.uint8)
