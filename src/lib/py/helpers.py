42f32fc735f9d500d9ffcd34bae0f47cc2acf15a
Carl Johnsen <carl-johannes@di.ku.dk>
2024-03-10 11:25:52 +0100

    Removed whitespaces in python helpers

--- src/lib/py/helpers.py
+++ src/lib/py/helpers.py
@@ -5,7 +5,7 @@ from config.paths import binary_root, hdf5_root
 import h5py
 from lib.cpp.cpu.io import load_slice
 import os
-import numpy as np 
+import numpy as np
 import pathlib
 import tqdm
 
@@ -33,7 +33,7 @@ def commandline_args(defaults):
                 sys.exit(1)
             else:
                 args.append(default)
-        else:                
+        else:
             args.append(type(default)(sys.argv[i+1]))
 
     return args
@@ -43,14 +43,14 @@ def update_hdf5(filename,group_name,datasets={},attributes={},dimensions=None,
                 compression=None,chunk_shape=None):
 
     output_dir = os.path.dirname(filename)
-    pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)    
+    pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
     f = h5py.File(filename,'a')
 
     if((group_name is not None) and (group_name != "/")):
         g = f.require_group(group_name)
     else:
        g = f
-    
+
     for k in datasets:
         v = datasets[k]
         if(k in g): del g[k]
@@ -81,16 +81,16 @@ def update_hdf5_mask(filename,group_name,datasets={},attributes={},dimensions=No
 
 def h5meta_info_volume_matched(sample):
     with h5py.File(f"{hdf5_root}/hdf5-byte/msb/{sample}.h5","r") as h5meta:
-        vm_shifts  = h5meta["volume_matching_shifts"][:]        
+        vm_shifts  = h5meta["volume_matching_shifts"][:]
         Nz, Ny, Nx = h5meta['voxels'].shape
         Nz -= np.sum(vm_shifts)
 
-        subvolume_dimensions =  h5meta['subvolume_dimensions'][:]                
+        subvolume_dimensions =  h5meta['subvolume_dimensions'][:]
         subvolume_nzs = subvolume_dimensions[:,0] - np.append(vm_shifts,0)
         voxel_size    = h5meta["voxels"].attrs["voxelsize"]
-        
+
         return ((Nz,Ny,Nx), subvolume_nzs, voxel_size)
-        
+
 def block_info(h5meta_filename,block_size=0, n_blocks=0,z_offset=0):
     print(f"Opening {h5meta_filename}")
     with h5py.File(h5meta_filename, 'r') as h5meta:
@@ -99,8 +99,7 @@ def block_info(h5meta_filename,block_size=0, n_blocks=0,z_offset=0):
         Nz -= np.sum(vm_shifts)
         Nr = int(np.sqrt((Nx//2)**2 + (Ny//2)**2))+1
 
-
-        subvolume_dimensions =  h5meta['subvolume_dimensions'][:]                
+        subvolume_dimensions =  h5meta['subvolume_dimensions'][:]
         subvolume_nzs = subvolume_dimensions[:,0] - np.append(vm_shifts,0)
 
         if block_size == 0:
@@ -109,23 +108,23 @@ def block_info(h5meta_filename,block_size=0, n_blocks=0,z_offset=0):
 
             # Do either n_blocks subvolumes, or if n_blocks == 0: all remaining after offset
             if n_blocks == 0:
-                n_blocks = len(subvolume_nzs)-z_offset 
-                
+                n_blocks = len(subvolume_nzs)-z_offset
+
         else:
-            blocks_are_subvolumes = False        
+            blocks_are_subvolumes = False
             if n_blocks == 0:
                 n_blocks = Nz // block_size + (Nz % block_size > 0)
 
 
-        return {'dimensions':(Nz,Ny,Nx,Nr),
-                'voxel_size':h5meta["voxels"].attrs["voxelsize"],
-                'n_blocks': n_blocks,
-                'block_size': block_size,
-                'blocks_are_subvolumes': blocks_are_subvolumes,
-                'subvolume_dimensions': subvolume_dimensions,
-                'subvolume_nzs': subvolume_nzs,
-                'subvolume_starts': np.concatenate([[0],np.cumsum(subvolume_nzs)[:-1]]
-                )
+        return {
+            'dimensions' : (Nz,Ny,Nx,Nr),
+            'voxel_size' :  h5meta["voxels"].attrs["voxelsize"],
+            'n_blocks' : n_blocks,
+            'block_size' : block_size,
+            'blocks_are_subvolumes' : blocks_are_subvolumes,
+            'subvolume_dimensions' : subvolume_dimensions,
+            'subvolume_nzs' : subvolume_nzs,
+            'subvolume_starts' : np.concatenate([[0],np.cumsum(subvolume_nzs)[:-1]])
         }
 
 
@@ -140,19 +139,19 @@ def load_block(sample, offset, block_size, mask_name, mask_scale, field_names):
     Nz, Ny, Nx = h5meta['voxels'].shape
     Nz -= np.sum(h5meta["volume_matching_shifts"][:])
     h5meta.close()
-#    print(block_size,Nz,offset)   
+#    print(block_size,Nz,offset)
     block_size       = min(block_size, Nz-offset)
 
     voxels = np.zeros((block_size,Ny,Nx),    dtype=np.uint16)
-    fields = np.zeros((Nfields,block_size//2,Ny//2,Nx//2), dtype=np.uint16)    
+    fields = np.zeros((Nfields,block_size//2,Ny//2,Nx//2), dtype=np.uint16)
 
     if mask_name is not None:
         for i in tqdm.tqdm(range(1),f"Loading {mask_name} mask from {hdf5_root}/masks/{mask_scale}x/{sample}.h5", leave=True):
             with h5py.File(f"{hdf5_root}/masks/{mask_scale}x/{sample}.h5","r") as h5mask:
                 mask = h5mask[mask_name]["mask"][offset//mask_scale:offset//mask_scale + block_size//mask_scale]
-            
+
     #TODO: Make voxel & field scale command line parameters
-    for i in tqdm.tqdm(range(1),f"Loading {voxels.shape} voxels from {binary_root}/voxels/1x/{sample}.uint16", leave=True):    
+    for i in tqdm.tqdm(range(1),f"Loading {voxels.shape} voxels from {binary_root}/voxels/1x/{sample}.uint16", leave=True):
         load_slice(voxels, f'{binary_root}/voxels/1x/{sample}.uint16', (offset, 0, 0), (Nz, Ny, Nx)) # TODO: Don't use 3 different methods for load/store
 
     for i in tqdm.tqdm(range(Nfields),f"Loading {binary_root}/fields/implant-{field_names}/2x/{sample}.npy",leave=True):
@@ -167,7 +166,7 @@ def load_block(sample, offset, block_size, mask_name, mask_scale, field_names):
         voxels[nz*mask_scale:] *= mask_1x[-1][NA,...]  # Remainder gets last line of mask
 
 #    plt.imshow(voxels[:,voxels.shape[1]//2,:]); plt.show()
-#    plt.imshow(fields[0,:,fields[0].shape[1]//2,:]); plt.show()    
+#    plt.imshow(fields[0,:,fields[0].shape[1]//2,:]); plt.show()
     return voxels, fields
 
 def row_normalize(A,r):
