a8bf8689742b4d3d14ba7043fc606b069f66e339
Carl Johnsen <carl-johannes@di.ku.dk>
2024-04-16 15:54:56 +0200

    #25 The pipeline has been revived! :eye: :lips: :eye:

--- src/processing_steps/1500_segment_blood_cc.py
+++ src/processing_steps/1500_segment_blood_cc.py
@@ -1,10 +1,14 @@
+import matplotlib
+matplotlib.use('Agg')
 import h5py, sys, os.path, pathlib, numpy as np, scipy.ndimage as ndi, tqdm, matplotlib.pyplot as plt
 sys.path.append(sys.path[0]+"/../")
 from config.constants import *
 from config.paths import hdf5_root, hdf5_root_fast, binary_root
-from lib.cpp.cpu import load_slice
+from lib.cpp.cpu_seq.io import load_slice, write_slice
 from scipy import ndimage as ndi
 from lib.py.helpers import block_info, commandline_args, update_hdf5
+from lib.py.resample import downsample2x, downsample3x
+import cupy as cp
 
 sample, m, scheme, chunk_size, verbose = commandline_args({"sample" : "<required>",
                                                            "material" : 0,
@@ -12,32 +16,40 @@ sample, m, scheme, chunk_size, verbose = commandline_args({"sample" : "<required
                                                            "chunk_size" : 256,
                                                            "verbose" : 1})
 
-scales = [32, 16, 8, 4, 2]
+#scales = [32, 16, 8, 4, 2]
+scales = [2]
 
 bi = block_info(f'{hdf5_root}/hdf5-byte/msb/{sample}.h5')
 Nz, Ny, Nx, _ = bi["dimensions"]
 
 for scale in tqdm.tqdm(scales, desc= 'Computing connected components'):
     data = f'{binary_root}/segmented/{scheme}/P{m}/{scale}x/{sample}.uint16'
-    output_dir = f'{hdf5_root_fast}/masks/{scale}x'
+    output_dir = f'{hdf5_root}/masks/{scale}x'
     pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
     nz, ny, nx = Nz // scale, Ny // scale, Nx // scale
     voxel_size = bi["voxel_size"]*scale
-    
-    voxels = np.empty((nz, ny, nx), dtype=np.uint16)    
+
+    voxels = np.empty((nz, ny, nx), dtype=np.uint16)
     load_slice(voxels, data, (0,0,0), (nz, ny, nx))
-    
+
 #    hist,bins = np.histogram(voxels.flatten(), 256)
 #    plt.plot(bins[11:],hist[10:]); plt.show()
 
 #    mask = voxels > 32000
-    mask = voxels > 40000    
-    
-    label, n_features = ndi.label(mask)
+    #mask = voxels > 40000
+
+    label, n_features = ndi.label(voxels)
     counts = np.bincount(label[label>0], minlength=n_features+1)
     largest_cc = np.argmax(counts)
     mask = (label == largest_cc)
 
+    if verbose >= 1:
+        print(f"Writing mask to {output_dir}/{sample}.h5")
+        plot_dir = f"{hdf5_root}/processed/blood_mask/"
+        pathlib.Path(plot_dir).mkdir(parents=True, exist_ok=True)
+        plt.imshow(mask[nz//2,:,:]); plt.savefig(f"{plot_dir}/{sample}_{scale}_yx.png"); plt.clf()
+        plt.imshow(mask[:,ny//2,:]); plt.savefig(f"{plot_dir}/{sample}_{scale}_zx.png"); plt.clf()
+        plt.imshow(mask[:,:,nx//2]); plt.savefig(f"{plot_dir}/{sample}_{scale}_zy.png"); plt.clf()
     update_hdf5(f"{output_dir}/{sample}.h5",
                 group_name=f"blood",
                 datasets={'mask':mask},
