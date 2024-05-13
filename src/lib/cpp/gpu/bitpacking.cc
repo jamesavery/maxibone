d830daba81fdac85f7f3b9ca58d1db094d3ef16e
Carl Johnsen <carl-johannes@di.ku.dk>
2024-05-13 16:09:04 +0200

    #39 Further optimized encoding by loading memory as 32-bit words, rather than bytes.

--- src/lib/cpp/gpu/bitpacking.cc
+++ src/lib/cpp/gpu/bitpacking.cc
@@ -11,37 +11,42 @@ namespace gpu {
     // 3. Coalesced shared memory access (A warp reads consecutive shared memory)
     // 4. Resolving bank conflicts in shared memory (By cyclically accessing a byte with an offset, rather than consecutive bytes)
     // 5. Bitwidth reduction (e.g. addresses are 32-bit when possible - this reduces both number of registers and number of instructions)
+    // 6. Loading 32 bits per thread, rather than 1 byte per thread (This reduces the number of instructions and memory transactions)
 
     template <typename T>
     void encode(const uint8_t *mask, const uint64_t n, T *packed) {
         constexpr uint8_t
             T_bits = sizeof(T)*8,
+            T_words = T_bits / sizeof(uint32_t),
             vec_size = 32,
+            vec_transactions = vec_size / T_words,
             worker_size = 1;
         constexpr uint32_t
-            buffer_size = (uint32_t)vec_size*(uint32_t)T_bits,
+            buffer_size = (uint32_t)vec_size*(uint32_t)T_words,
             block_size = (uint32_t)worker_size*buffer_size;
 
-        uint32_t n_blocks = (uint32_t) ((n + (uint64_t)block_size - 1) / (uint64_t)block_size);
+        uint32_t n_blocks = (uint32_t) ((n/sizeof(uint32_t) + (uint64_t)block_size - 1) / (uint64_t)block_size);
+
+        const uint32_t *mask32 = (const uint32_t *) mask;
 
         // TODO handle block_size unalignment
 
-        #pragma acc data copyin(mask[0:n]) copyout(packed[0:n/(uint64_t)T_bits])
+        #pragma acc data copyin(mask32[0:n/sizeof(uint32_t)]) copyout(packed[0:n/(uint64_t)T_bits])
         #pragma acc parallel vector_length(vec_size) num_workers(worker_size)
         {
             #pragma acc loop gang
             for (uint32_t i = 0; i < n_blocks; i++) {
                 #pragma acc loop worker
                 for (uint32_t j = 0; j < worker_size; j++) {
-                    uint32_t local[(uint16_t)vec_size*(uint16_t)T_bits]; // Shared memory
-                    uint64_t offset = ((uint64_t)i * (uint64_t)block_size) + (uint64_t)j*(uint64_t)vec_size*(uint64_t)T_bits;
+                    uint32_t local[buffer_size]; // Shared memory
+                    uint64_t offset = ((uint64_t)i * (uint64_t)block_size) + (uint64_t)j*(uint64_t)vec_size*(uint64_t)T_words;
                     #pragma acc cache(local)
                     {
                         // Load mask into shared memory with coalesced access to mask
                         #pragma acc loop vector
                         for (uint32_t k = 0; k < vec_size; k++) {
-                            for (uint32_t l = 0; l < T_bits; l++) {
-                                local[(uint16_t)l*(uint16_t)vec_size + (uint16_t)k] = (uint32_t) mask[offset + (uint64_t)l*(uint64_t)vec_size + (uint64_t)k];
+                            for (uint32_t l = 0; l < T_words; l++) {
+                                local[(k%(uint16_t)T_words)*(uint16_t)vec_size + (k/(uint16_t)T_words)+l*(uint16_t)vec_transactions] = mask32[offset + (uint64_t)l*(uint64_t)vec_size + (uint64_t)k];
                             }
                         }
 
@@ -49,12 +54,16 @@ namespace gpu {
                         #pragma acc loop vector
                         for (uint32_t k = 0; k < vec_size; k++) {
                             T packed_value = 0;
-                            for (uint32_t l = 0; l < T_bits; l++) {
-                                uint8_t bank_offset = (uint8_t) (((uint16_t)k+(uint16_t)l) % (uint16_t)T_bits);
-                                uint32_t val = local[(uint16_t)k*(uint16_t)T_bits + (uint16_t)bank_offset] & 1;
-                                packed_value |= val << (((uint16_t)T_bits-1)-((uint16_t)bank_offset));
+                            for (uint32_t l = 0; l < T_words; l++) {
+                                uint32_t val = local[(uint16_t)l*(uint16_t)vec_size + (uint16_t)k];
+                                uint32_t local_packed =
+                                    (val & 0x00000001) << 3 |
+                                    (val & 0x00000100) >> 6 |
+                                    (val & 0x00010000) >> 15 |
+                                    (val & 0x01000000) >> 24;
+                                packed_value |= local_packed << (((uint16_t)T_bits-4) - l*4);
                             }
-                            packed[offset/(uint64_t)T_bits + (uint64_t)k] = packed_value;
+                            packed[offset/(uint64_t)T_words + (uint64_t)k] = packed_value;
                         }
                     }
                 }
