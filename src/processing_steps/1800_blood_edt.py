362edac18be51244b7d4044250974c889c68c1cf
Carl Johnsen <carl-johannes@di.ku.dk>
2024-06-26 05:58:36 +0000

    Added plotting to computing bone health

--- src/processing_steps/1800_blood_edt.py
+++ src/processing_steps/1800_blood_edt.py
@@ -16,43 +16,85 @@ import os
 from tqdm import tqdm
 
 if __name__ == '__main__':
-    sample, scale, m, scheme, threshold_prob, threshold_distance = commandline_args({
+    sample, scale, m, scheme, threshold_prob, threshold_distance, verbose = commandline_args({
         "sample" : "<required>",
         "scale" : 1,
         "material": 0,
         "scheme": "gauss+edt",
         "threshold_prob" : 0, # For whether voxel is blood or not
-        "threshold_distance" : 100
+        "threshold_distance" : 5,
+        'verbose': 1
     })
 
     probs_dir = f'{binary_root}/segmented/{scheme}'
     soft_path = f'{probs_dir}/P{m}/{scale}x/{sample}.uint16'
     bone_path = f'{probs_dir}/P{np.abs(m-1)}/{scale}x/{sample}.uint16'
     output_dir = f"{binary_root}/fields/soft-tissue-edt/{scale}x"
+    image_output_dir = f"{hdf5_root}/processed/soft-tissue-edt/{scale}x/{sample}"
+
+    if verbose >= 1: os.makedirs(image_output_dir, exist_ok=True)
 
     bi = block_info(f'{hdf5_root}/hdf5-byte/msb/{sample}.h5')
     shape = np.array(bi["dimensions"][:3])
     shape //= scale
+    nz, ny, nx = shape
 
     os.makedirs(output_dir, exist_ok=True)
 
     soft = np.memmap(soft_path, dtype=np.uint16, mode='r').reshape(shape)
 
-    soft_threshed = ~(soft > threshold_prob)
+    soft_threshed = (soft > threshold_prob)
     del soft
+
+    if verbose >= 1:
+        print (f'Writing soft tissue debug plane images to {image_output_dir}')
+        names = ['yx', 'zx', 'zy']
+        planes = [soft_threshed[nz//2,:,:], soft_threshed[:,ny//2,:], soft_threshed[:,:,nx//2]]
+        for name, plane in zip(names, planes):
+            plt.imshow(plane)
+            plt.savefig(f'{image_output_dir}/{sample}_soft_{name}.png')
+            plt.clf()
 
     hyperthreading = True
     n_cores = mp.cpu_count() // (2 if hyperthreading else 1) # Only count physical cores
     edted = edt.edt(soft_threshed, parallel=n_cores)
     del soft_threshed
 
+    if verbose >= 1:
+        print (f'Writing EDT debug plane images to {image_output_dir}')
+        names = ['yx', 'zx', 'zy']
+        planes = [edted[nz//2,:,:], edted[:,ny//2,:], edted[:,:,nx//2]]
+        for name, plane in zip(names, planes):
+            plt.imshow(plane)
+            plt.colorbar()
+            plt.savefig(f'{image_output_dir}/{sample}_edt_{name}.png')
+            plt.clf()
+
     disted = edted < threshold_distance
     del edted
+
+    if verbose >= 1:
+        print (f'Writing thresholded EDT debug plane images to {image_output_dir}')
+        names = ['yx', 'zx', 'zy']
+        planes = [disted[nz//2,:,:], disted[:,ny//2,:], disted[:,:,nx//2]]
+        for name, plane in zip(names, planes):
+            plt.imshow(plane)
+            plt.savefig(f'{image_output_dir}/{sample}_dist_{name}.png')
+            plt.clf()
 
     bone = np.memmap(bone_path, dtype=np.uint16, mode='r').reshape(shape)
 
     bone_threshed = bone > threshold_prob
     del bone
+
+    if verbose >= 1:
+        print (f'Writing bone debug plane images to {image_output_dir}')
+        names = ['yx', 'zx', 'zy']
+        planes = [bone_threshed[nz//2,:,:], bone_threshed[:,ny//2,:], bone_threshed[:,:,nx//2]]
+        for name, plane in zip(names, planes):
+            plt.imshow(plane)
+            plt.savefig(f'{image_output_dir}/{sample}_bone_{name}.png')
+            plt.clf()
 
     bone_count = np.sum(bone_threshed)
     dist_count = np.sum(bone_threshed & disted)
