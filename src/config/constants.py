668c507e622aecc033cf8b58f76b45e8708f4b25
Carl Johnsen <carl-johannes@di.ku.dk>
2024-08-05 15:21:09 +0000

    #40 Added osteocyte_ prefix to constants related to the osteocyte mask.

--- src/config/constants.py
+++ src/config/constants.py
@@ -8,9 +8,9 @@ implant_threshold_byte = int(((implant_threshold-global_vmin)/(global_vmax-globa
 # Osteocytes have a stellate shape, approximately 7 micrometers deep and wide by 15 micrometers in length.[3]
 # The cell body varies in size from 5–20 micrometers in diameter and contain 40–60 cell processes per cell,
 # [4] with a cell to cell distance between 20–30 micrometers.[3]
-minmax_buffer = 1.25
-osteocyte_diam_min = 5/minmax_buffer
-osteocyte_diam_max = 20*minmax_buffer
+osteocyte_minmax_buffer = 1.25
+osteocyte_diam_min = 5/osteocyte_minmax_buffer
+osteocyte_diam_max = 20*osteocyte_minmax_buffer
 osteocyte_lmin = osteocyte_diam_min/np.sqrt(3)  # Smallest possible side-length: assuming box-shaped osteocyte of minimum diameter
 osteocyte_lmax = osteocyte_diam_max/np.sqrt(3)
 osteocyte_Vmin = osteocyte_lmin**3
