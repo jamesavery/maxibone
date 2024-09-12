0e07f9d1683f75bc8aa43d7a470bb8b66a790fe6
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-12 08:47:11 +0200

    #34 Added docstrings for the config module.

---
+++ src/config/__init__.py
@@ -0,0 +1,5 @@
+'''
+Module for holding global configuration variables used throughout the Osteomorph project. It is divided into two submodules:
+- `paths`: Global paths for the Osteomorph project.
+- `constants`: Global constants for the Osteomorph project.
+'''
