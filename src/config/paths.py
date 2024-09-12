66fe4502f2d161c5cebbe1a4e43485e087678d9a
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-12 08:34:29 +0200

    #34 Added docstrings to config.paths.

--- src/config/paths.py
+++ src/config/paths.py
@@ -1,12 +1,26 @@
+'''
+Global paths for the Osteomorph project.
+'''
+# TODO fix issue 41 in here, so that it easily trickles down to the rest of the codebase.
 data_root = "/data"
+'''The root directory for the data on potentially slow, but large, storage.'''
 fast_root = "/data_fast"
+'''The root directory for the data on fast(er), but smaller, storage.'''
 
 hdf5_root        = f"{data_root}/MAXIBONE/Goats/tomograms"
+'''The root directory for the HDF5 data on potentially slow, but large, storage.'''
 hdf5_root_fast   = f"{fast_root}/MAXIBONE/Goats/tomograms"
+'''The root directory for the HDF5 data on fast(er), but smaller, storage.'''
 binary_root      = f"{hdf5_root}/binary"
+'''The root directory for the binary data on potentially slow, but large, storage.'''
 binary_root_fast = f"{hdf5_root_fast}/binary"
+'''The root directory for the binary data on fast(er), but smaller, storage.'''
 
 esrf_data_local     = f"{hdf5_root}/ESRF/"
+'''The root directory for the local copy of ESRF data.'''
 esrf_data_sftp      =  "/XNS/XrayImaging/MiG/manjula.esci.nbi.dk.2_localhost/"
+'''The root directory for the SFTP copy of ESRF data.'''
 esrf_implants_root  = f"{esrf_data_local}/esrf_dental_implants_april_2013/"
+'''The root ESRF directory of the implant data.'''
 esrf_granules_root  = f"{esrf_data_local}/esrf_dental_granules_july_2012/"
+'''The root ESRF directory of the granule data.'''
