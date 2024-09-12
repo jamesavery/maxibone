4ce3ed5670d80c3d3963a49d713908fe5e4f8291
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-12 11:21:30 +0200

    #12 General cleanup in lib.py.distributions

--- src/lib/py/distributions.py
+++ src/lib/py/distributions.py
@@ -25,6 +25,8 @@ def gaussian(xs, a, b, c):
         An array of Gaussian function values, I.e., `G[i] = a*np.exp(-b*(xs[i]-c)**2)`
     '''
 
+    return (a*a) * np.exp(-(b*b) * ((xs-c)**2))
+
 
 def gaussians(xs, abcd):
     '''
@@ -44,12 +46,11 @@ def gaussians(xs, abcd):
     '''
 
     n = len(abcd) // 4
-    A = abcd[0  :  n,na]**2    # a^2 to enforce positive values
-    B = abcd[n  :2*n,na]**2    # b^2 to enforce positive values
-    C = abcd[2*n:3*n,na]       # center is allowed to be negative
-    
-    X       = xs[na,:]
-    G       = A*np.exp(-B*((X-C)**2));
+    A = abcd[0  :  n,na]**2 # a^2 to enforce positive values
+    B = abcd[n  :2*n,na]**2 # b^2 to enforce positive values
+    C = abcd[2*n:3*n,na]    # center is allowed to be negative
+    X = xs[na,:]
+    G = A * np.exp(-B * ((X-C)**2))
     return G
 
 
@@ -70,10 +71,10 @@ def exponentials(xs, abcd):
         I.e., `E[i,j] = a[i]*np.exp(-b[i]*(xs[j]-c[i]))`.
     '''
 
+    n       = len(abcd) // 4
     A, B, C = abcd[:n,na]**2, abcd[n:2*n,na]**2, abcd[2*n:,na]
-    
     X       = xs[na,:]
-    E       =  A*np.exp(-B*np.abs(X-C));
+    E       = A * np.exp(-B * np.abs(X-C))
     return E
 
 
@@ -94,9 +95,10 @@ def lorentzians(xs, abcd):
         I.e., `L[i,j] = a[i]*b[i]**2 / ((xs[j]-c[i])**2 + b[i]**2)`.
     '''
 
+    n            = len(abcd) // 4
     A, gamma2, C = abcd[:n,na]**2, abcd[n:2*n,na]**2, abcd[2*n:,na]
-    X       = xs[na,:]
-    L       = A*gamma2/(((X-C)**2) + gamma2)
+    X            = xs[na,:]
+    L            = A * gamma2 / (((X-C)**2) + gamma2)
     return L
 
 def powers(xs, abcd):
@@ -116,11 +118,14 @@ def powers(xs, abcd):
         I.e., `P[i,j] = a[i]**2 * np.exp(-b[i]**2 * np.abs(xs[j]-c[i])**d)`.
     '''
 
+    n          = len(abcd) // 4
+    A, B, C, D = abcd[:n,na], abcd[n:2*n,na], abcd[2*n:3*n,na], abcd[3*n:,na]
     X          = xs[na,:]
-    P          = (A*A)*np.exp(-(B*B)*np.abs(X-C)**(D*D))
+    P          = (A*A) * np.exp(-(B*B) * np.abs(X-C)**(D*D))
     return P
 
 
+def b_from_width(f, w):
     '''
     Returns the `b` parameter for a given width `w` of a Gaussian, exponential, power, or Lorentzian distribution.
 
@@ -137,3 +142,11 @@ def powers(xs, abcd):
         The calculated `b` parameter.
     '''
 
+    if f == gaussians:
+        return 6 / w
+    if f == exponentials:
+        return np.sqrt(2 / w)
+    if f == powers:
+        return np.sqrt(2 / w)
+    if f == lorentzians:
+        return w / 4
