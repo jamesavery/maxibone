d37ce17f5e94693f516d96f3f87429abc0a01b0b
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-13 10:54:22 +0200

    #12 General cleanup in lib.py.helpers.

--- src/lib/py/helpers.py
+++ src/lib/py/helpers.py
@@ -47,12 +47,12 @@ def commandline_args(defaults):
 
     helpstring = f"Syntax: {sys.argv[0]} "
     for k in keys:
-        if(defaults[k] == "<required>"): helpstring += f" <{k}>"
-        else:                            helpstring += f" [{k}:{defaults[k]}]"
+        if (defaults[k] == "<required>"): helpstring += f" <{k}>"
+        else:                             helpstring += f" [{k}:{defaults[k]}]"
 
     # Do we just want to know how to call this script?
-    if(len(sys.argv)==2):
-        if(sys.argv[1] == "--help" or sys.argv[1] == "-h"):
+    if (len(sys.argv) == 2):
+        if (sys.argv[1] == "--help" or sys.argv[1] == "-h"):
             print(helpstring, file=sys.stderr)
             sys.exit(1)
 
@@ -60,7 +60,7 @@ def commandline_args(defaults):
     args = []
     for i in range(len(keys)):
         default = defaults[keys[i]]
-        if(len(sys.argv)<=i+1):
+        if (len(sys.argv) <= i+1):
             if(default == "<required>"):
                 print(helpstring, file=sys.stderr)
                 sys.exit(1)
@@ -90,6 +90,8 @@ def generate_cylinder_mask(nx):
     rs = np.sqrt(xs[np.newaxis,np.newaxis,:]**2 + xs[np.newaxis,:,np.newaxis]**2)
     return rs <= 1
 
+def update_hdf5(filename, group_name, datasets={}, attributes={}, dimensions=None,
+                compression=None, chunk_shape=None):
     '''
     Update an HDF5 file with new datasets and attributes.
 
@@ -115,22 +117,26 @@ def generate_cylinder_mask(nx):
     None
     '''
 
+    # Create the file and directory if it doesn't exist
     output_dir = os.path.dirname(filename)
     pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
     f = h5py.File(filename,'a')
 
+    # Create the group if it doesn't exist
     if((group_name is not None) and (group_name != "/")):
         g = f.require_group(group_name)
     else:
        g = f
 
+    # Update datasets
     for k in datasets:
         v = datasets[k]
         if(k in g): del g[k]
-        g.create_dataset(k,shape=v.shape,dtype=v.dtype,
-                          compression=compression, chunks=chunk_shape,maxshape=None)
+        g.create_dataset(k, shape=v.shape, dtype=v.dtype,
+                          compression=compression, chunks=chunk_shape, maxshape=None)
         g[k][:] = v[:]
 
+        # Update dimensions if provided
         if dimensions is not None:
             try:
                 dims = dimensions[k]
@@ -139,14 +145,15 @@ def generate_cylinder_mask(nx):
             except:
                 pass
 
+    # Update attributes
     for k in attributes:
         v = attributes[k]
         g.attrs[k] = v
 
     f.close()
 
-
-#TODO: Use this for masks, no compression and no chunking default for small metadata datasets
+def update_hdf5_mask(filename, group_name, datasets={}, attributes={}, dimensions=None,
+                     compression="lzf", chunk_shape=None):
     '''
     Update an HDF5 file with new datasets and attributes.
     This function is specifically for updating masks, and uses lzf compression and no chunking by default.
@@ -173,6 +180,7 @@ def generate_cylinder_mask(nx):
     None
     '''
 
+    update_hdf5(filename, group_name, datasets, attributes, dimensions, compression, chunk_shape)
 
 def h5meta_info_volume_matched(sample):
     '''
@@ -189,16 +197,18 @@ def h5meta_info_volume_matched(sample):
         A tuple containing the volume-matched dimensions, subvolume dimensions, and voxel size for the sample.
     '''
 
+    with h5py.File(f"{hdf5_root}/hdf5-byte/msb/{sample}.h5", "r") as h5meta:
         vm_shifts  = h5meta["volume_matching_shifts"][:]
         Nz, Ny, Nx = h5meta['voxels'].shape
         Nz -= np.sum(vm_shifts)
 
-        subvolume_dimensions =  h5meta['subvolume_dimensions'][:]
-        subvolume_nzs = subvolume_dimensions[:,0] - np.append(vm_shifts,0)
-        voxel_size    = h5meta["voxels"].attrs["voxelsize"]
+        subvolume_dimensions = h5meta['subvolume_dimensions'][:]
+        subvolume_nzs        = subvolume_dimensions[:,0] - np.append(vm_shifts,0)
+        voxel_size           = h5meta["voxels"].attrs["voxelsize"]
 
         return ((Nz,Ny,Nx), subvolume_nzs, voxel_size)
 
+def block_info(h5meta_filename, scale, block_size=0, n_blocks=0, z_offset=0):
     '''
     Returns information about the blocks in a volume-matched dataset. It is used for loading blocks in the `load_block` function.
 
@@ -240,15 +250,13 @@ def h5meta_info_volume_matched(sample):
             # Do either n_blocks subvolumes, or if n_blocks == 0: all remaining after offset
             if n_blocks == 0:
                 n_blocks = len(subvolume_nzs)-z_offset
-
         else:
             blocks_are_subvolumes = False
             if n_blocks == 0:
                 n_blocks = Nz // block_size + (Nz % block_size > 0)
 
-
         return {
-            'dimensions' : (Nz,Ny,Nx,Nr),
+            'dimensions' : (Nz, Ny, Nx, Nr),
             'voxel_size' :  h5meta["voxels"].attrs["voxelsize"],
             'n_blocks' : n_blocks,
             'block_size' : block_size,
@@ -257,7 +265,6 @@ def h5meta_info_volume_matched(sample):
             'subvolume_nzs' : subvolume_nzs,
             'subvolume_starts' : np.concatenate([[0],np.cumsum(subvolume_nzs)[:-1]])
         }
-
 
 def load_block(sample, scale, offset, block_size, mask_name, mask_scale, field_names, field_scale):
     '''
@@ -294,7 +301,6 @@ def load_block(sample, scale, offset, block_size, mask_name, mask_scale, field_n
 
     NA = np.newaxis
     Nfields = len(field_names)
-
     h5meta = h5py.File(f'{hdf5_root}/hdf5-byte/msb/{sample}.h5', 'r')
     Nz1x, Ny1x, Nx1x = h5meta['voxels'].shape
     Nz1x -= np.sum(h5meta["volume_matching_shifts"][:])
@@ -303,20 +309,19 @@ def load_block(sample, scale, offset, block_size, mask_name, mask_scale, field_n
     mNz, mNy, mNx = Nz1x // mask_scale, Ny1x // mask_scale, Nx1x // mask_scale
     mask_scale_relative = mask_scale // scale
     h5meta.close()
-#    print(block_size,Nz,offset)
-    block_size       = min(block_size, Nz-offset)
+    block_size = min(block_size, Nz-offset)
 
-    voxels = np.zeros((block_size,Ny,Nx),    dtype=np.uint16)
-    fields = np.zeros((Nfields,block_size//field_scale,fNy,fNx), dtype=np.uint16)
+    voxels = np.zeros((block_size, Ny, Nx), dtype=np.uint16)
+    fields = np.zeros((Nfields, block_size//field_scale, fNy, fNx), dtype=np.uint16)
 
     if mask_name is not None:
         for i in tqdm.tqdm(range(1),f"Loading {mask_name} mask from {hdf5_root}/masks/{mask_scale}x/{sample}.h5"):
             with h5py.File(f"{hdf5_root}/masks/{mask_scale}x/{sample}.h5","r") as h5mask:
                 mask = h5mask[mask_name]["mask"][offset//mask_scale_relative:offset//mask_scale_relative + block_size//mask_scale_relative]
 
-    #TODO: Make voxel & field scale command line parameters
     for i in tqdm.tqdm(range(1),f"Loading {voxels.shape} voxels from {binary_root}/voxels/{scale}x/{sample}.uint16", leave=True):
-        load_slice(voxels, f'{binary_root}/voxels/{scale}x/{sample}.uint16', (offset, 0, 0), (block_size, Ny, Nx)) # TODO: Don't use 3 different methods for load/store
+        # TODO: Don't use 3 different methods for load/store
+        load_slice(voxels, f'{binary_root}/voxels/{scale}x/{sample}.uint16', (offset, 0, 0), (block_size, Ny, Nx))
 
     for i in tqdm.tqdm(range(Nfields),f"Loading {binary_root}/fields/implant-{field_names}/{field_scale}x/{sample}.npy",leave=True):
         fi = np.load(f"{binary_root}/fields/implant-{field_names[i]}/{field_scale}x/{sample}.npy", mmap_mode='r')
@@ -324,15 +329,14 @@ def load_block(sample, scale, offset, block_size, mask_name, mask_scale, field_n
 
     if mask_name is not None:
         nz, ny, nx = (block_size//mask_scale_relative), Ny//mask_scale_relative, Nx//mask_scale_relative
-        mask_1x = np.broadcast_to(mask[:,NA,:,NA,:,NA],(nz,mask_scale_relative, ny,mask_scale_relative, nx,mask_scale_relative))
-        mask_1x = mask_1x.reshape(nz*mask_scale_relative,ny*mask_scale_relative,nx*mask_scale_relative)
-        voxels[:nz*mask_scale_relative] *= mask_1x               # block_size may not be divisible by mask_scale_relative
-        voxels[nz*mask_scale_relative:] *= mask_1x[-1][NA,...]  # Remainder gets last line of mask
+        mask_1x = np.broadcast_to(mask[:,NA,:,NA,:,NA],(nz, mask_scale_relative, ny, mask_scale_relative, nx, mask_scale_relative))
+        mask_1x = mask_1x.reshape(nz*mask_scale_relative, ny*mask_scale_relative, nx*mask_scale_relative)
+        voxels[:nz*mask_scale_relative] *= mask_1x             # block_size may not be divisible by mask_scale_relative
+        voxels[nz*mask_scale_relative:] *= mask_1x[-1][NA,...] # Remainder gets last line of mask
 
-#    plt.imshow(voxels[:,voxels.shape[1]//2,:]); plt.show()
-#    plt.imshow(fields[0,:,fields[0].shape[1]//2,:]); plt.show()
     return voxels, fields
 
+def row_normalize(A, r):
     '''
     Normalize the rows of a matrix A by the vector r.
 
@@ -350,8 +354,9 @@ def load_block(sample, scale, offset, block_size, mask_name, mask_scale, field_n
     '''
 
     na = np.newaxis
-    return A/(r[:,na]+(r==0)[:,na])
+    return A / (r[:,na] + (r==0)[:,na])
 
+def to_int_py(x, dtype):
     '''
     Convert a numpy array to an integer type.
     This is a pure Python implementation, which is slower than the C++ implementation in `to_int`.
@@ -383,6 +388,7 @@ def load_block(sample, scale, offset, block_size, mask_name, mask_scale, field_n
     result += 1
     return result
 
+def to_int(x, dtype):
     '''
     Convert a numpy array to an integer type.
     This is a wrapper around the C++ implementation in `lib/cpp/*/general.cc`.
@@ -405,5 +411,5 @@ def load_block(sample, scale, offset, block_size, mask_name, mask_scale, field_n
         return to_int_py(x,dtype)
 
     result = np.empty(x.shape, dtype=dtype)
-    normalized_convert(x,result)
+    normalized_convert(x, result)
     return result
