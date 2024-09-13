4225f78bcdc39935f85771dd7956ef7167b90ece
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-13 11:58:22 +0200

    #12 General cleanup in lib.py.piecewise_cubic

--- src/lib/py/piecewise_cubic.py
+++ src/lib/py/piecewise_cubic.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python3
 '''
 This module contains functions for generating Gaussian, exponential, power, and Lorentzian distributions.
 
@@ -28,32 +29,6 @@ So we have 4+2*(N-1) = 2*(N+1) variables to determine: A1, B1, C1, D1, and C2, D
 from numpy import array, linspace, sin, empty, zeros, linalg, random, pad, concatenate
 import numpy as np
 import numpy.linalg as la
-# Scheme:
-#  Fit an N-segment piecewise cubic polynomial to a set of points with linear least squares with
-#  two exact conditions:
-#   (1) Continuity at the borders:        f_n (X_n) = f_{n-1} (X_n)
-#   (2) Differentiability at the borders: f_n'(X_n) = f'_{n-1}(X_n)
-#
-#  Input: A list of M data points [(x1,y1),....(x_M,y_M)] and
-#         A list of N+1 borders   [X0,X1,....,X_N]
-#
-#  This file generalizes the 2-segment method given in cubic2.py to an arbitrary
-#  number of segments.
-#           / f1(x) = A1 + B1*(x-X0) + C1*(x-X0)**2 + D1*(x-X0)**3          if x>=X0 & x<=X1
-#    f(x) = |
-#           | f2(x) = f1(X1) + f1'(X1)*(x-X2) + C2*(x-X2)**2 + D2*(x-X2)**3 if x>=X1 & x<=X2
-#           |
-#           | f3(x) = f2(X2) + f2'(X2)*(x-X3) + C2*(x-X3)**2 + D2*(x-X3)**3 if x>=X2 & x<=X3
-#           |   ...
-#           \ f_N(x) = f_{N-1}(X_{N-1}) + f'_{N-1}(X_{N-1})*(x-X_N) + C_N*(x-X_N)**2 + D_N*(x-X_N)**3
-#                                                                           if x>=X_{N-1} & x<= X_N
-#
-# We still want to set up a linear least squares system of equations that implicitly
-# obeys the two exact conditions (so they are not weakened by the least squares
-# approximation to the over-determined system).
-
-# Eq. (1) and (2) determine A_n (the function value at X_n) and B_n (the derivative at X_n).
-# So we have 4+2*(N-1) = 2*(N+1) variables to determine: A1, B1, C1, D1, and C2, D2, C3, D3, ..., C_N, D_N
 
 # Each data point (x_i,y_i)  defines a row with the coefficients for A1,B1,C1,D1,C2,D2,...,C_N,D_N in the matrix, and y_i on the RHS.
 def mxrow_f (x, borders, verbose=False):
@@ -76,21 +51,25 @@ def mxrow_f (x, borders, verbose=False):
         The matrix row for the function value `f_n(x) = A + B*(x-Xn) + C*(x-Xn)**2 + D*(x-Xn)**3`.
     '''
 
+    if (len(borders) < 2):
         print(f"No segments: borders = {borders}")
         return
 
     Xleft, Xright = borders[-2:] # x >= Xleft & x <= Xright
+    n = len(borders)-2          # n is the current segment number
     if verbose:
         print(f"f_{n}({x}): Xleft, Xright = {np.round([Xleft, Xright], 2)}; borders = {borders}, {len(borders)-1} segments")
 
-    if n==0:
+    if n == 0:
         X = x-Xleft
-        return array([ 1, X, X**2,  X**3])
+        return array([ 1, X, X**2,  X**3 ])
     else:
         # We recursively define the matrix row
         # f_n(x) = f_{n-1}(Xn) + f'_{n-1}(Xn)*(x-Xn) + Cn*(x-Xn)**2 + Dn*(x-Xn)**3
         if verbose:
             print(f"Recursing down from level {n} to level {n-1}, evaluated at {Xleft}")
+        row               = zeros((4 + 2*n,), dtype=float)
+        row[:(4+2*(n-1))] = mxrow_f(Xleft, borders[:-1]) + mxrow_df(Xleft, borders[:-1]) * (x-Xleft)
         row[-2:]          = [(x-Xleft)**2, (x-Xleft)**3]
         if verbose:
             print(f"returning length = {len(row)} row at level {n}")
@@ -121,17 +100,18 @@ def mxrow_df(x, borders, verbose=False):
         return
 
     Xleft, Xright = borders[-2:] # x >= Xleft & x <= Xright
+    n = len(borders)-2           # n is the current segment number
     if verbose:
         print(f"f'_{n}({x}): Xleft,Xright = {np.round([Xleft, Xright], 2)}; borders = {borders}, {len(borders)-1} segments")
 
     if n==0:
         X0 = borders[0]
-        return array([ 0,    1, 2*(x-X0) ,3*(x-X0)**2])
+        return array([ 0, 1, 2 * (x-X0) ,3 * (x-X0)**2 ])
     else:
         # We recursively define the matrix row: f'_n(x) = f'_{n-1}(Xn) + Cn*2*(x-Xn) + Dn*3*(x-Xn)
-        row               = zeros((4+2*n,), dtype=float)
+        row               = zeros((4 + 2*n,), dtype=float)
         row[:(4+2*(n-1))] = mxrow_df(Xleft, borders[:-1], verbose)
-        row[-2:]          = [2*(x-Xleft), 3*((x-Xleft)**2)]
+        row[-2:]          = [2 * (x-Xleft), 3 * ((x-Xleft)**2)]
         return row
 
 # TODO:
@@ -160,16 +140,19 @@ def piecewisecubic_matrix(xs, ys, Xs, verbose=False):
         The matrix `A` and the RHS vector `b` for the linear least squares problem.
     '''
 
+    M = len(xs)   # M data points
+    N = len(Xs)-1 # N segments, i.e. N+1 borders
 
-    A = zeros((M,2*(N+1)),dtype=float)
-    b = zeros((M,1),dtype=float)
+    A = zeros((M, 2 * (N+1)), dtype=float)
+    b = zeros((M, 1),         dtype=float)
 
-    n = 0                        # Start in first region
+    n = 0 # Start in first region
     Xleft, Xright = Xs[0], Xs[1]
 
     for i in range(len(xs)):
         if verbose:
             print(f"Xleft, Xright = {Xleft, Xright}, Xs = {Xs}, n = {n}")
+        if (xs[i] > Xright):
             n += 1
             Xleft, Xright = Xs[n], Xs[n+1]
 
@@ -218,7 +201,7 @@ def piecewisecubic(pc, all_xs, extrapolation="cubic"):
         # evaluate dy/dx at domain edge Xs.min()
         ys   += [A + B*xs]      # A is value at Xmin, B is slope at Xmin
     if extrapolation=="constant":
-        ys += [np.ones(xs_left_of_domain.shape)*A] # A is value at Xmin
+        ys += [np.ones(xs_left_of_domain.shape) * A] # A is value at Xmin
 
     # Process points inside domain
     for n in range(N):
@@ -227,16 +210,14 @@ def piecewisecubic(pc, all_xs, extrapolation="cubic"):
         Xleft, Xright = Xs[n], Xs[n+1]
         xs_segment    = all_xs[(all_xs>=Xleft) & (all_xs< Xright)]
 
-
         # f_n(x) = A + B*(x-Xn) + C*(x-Xn)**2 + D*(x-Xn)**3
-        xs   = xs_segment - Xleft                 # Segment coordinate x-Xn
+        xs  = xs_segment - Xleft                  # Segment coordinate x-Xn
         ys += [A + B*xs + C*(xs**2) + D*(xs**3)]  # f_n(xs)
 
         # Calculate next A and B coefficients:
         X = Xright-Xleft
         A = A + B*X + C*(X**2) + D*(X**3)   # A_{n+1} = f_n (X_{n+1})
         B =     B   + C*2*X    + D*3*(X**2) # B_{n+1} = f'_n(X_{n+1})
-
 
     # Process points outside the domain, right side
     Xmax = Xs[-1]
@@ -279,16 +260,20 @@ def fit_piecewisecubic(xs, ys, Xs, regularization_beta=0, verbose=False):
 
     if regularization_beta>0:
         m,n = A.shape
+        I          = np.eye(n, n)[3:]
+        I[:,5:]   -= np.eye(n-3, n-3)[:,:-2]
+        I[:-2,7:] -= np.eye(n-5, n-5)[:,:-2]
         if verbose:
             print(I)
         Ap  = np.vstack([A,regularization_beta*I])
         bp  = np.concatenate([b,np.zeros((n-3,1))])
         if verbose:
             print(f"{regularization_beta} {m,n} {A.shape}, {I.shape}, {Ap.shape}, b:{b.shape}, bp:{bp.shape}")
+        coefs, residuals, rank, sing = linalg.lstsq(Ap, bp, rcond=None)
     else:
-        coefs, residuals, rank, sing = linalg.lstsq(A,b,rcond=None)
+        coefs, residuals, rank, sing = linalg.lstsq(A, b, rcond=None)
 
-    return (coefs.reshape(-1),Xs)
+    return (coefs.reshape(-1), Xs)
 
 def smooth_fun(xs, ys, n_segments, regularization_beta=0, verbose=False):
     '''
@@ -314,6 +299,7 @@ def smooth_fun(xs, ys, n_segments, regularization_beta=0, verbose=False):
         The polynomial coefficients and the borders of the segments.
     '''
 
+    borders = linspace(xs.min(), xs.max()+1, n_segments)
 
     return fit_piecewisecubic(xs, ys, borders, regularization_beta, verbose)
 
@@ -340,7 +326,6 @@ if __name__ == "__main__":
     # #plt.axvline(x=xs[m])
     # plt.show()
 
-
     f = np.load("test_data/output_curve_values.npy")
 
     vals = f[7,:,1].T
@@ -348,22 +333,22 @@ if __name__ == "__main__":
 
     xs      = np.argwhere(mask).astype(float).flatten()
     ys      = vals[mask]
-    borders = np.linspace(xs.min(), xs.max()+1,5)
+    borders = np.linspace(xs.min(), xs.max() + 1,5)
     A, b    = piecewisecubic_matrix(xs, ys, borders, verbose)
 
-    coefs, residuals, rank, sing = linalg.lstsq(A,b,rcond=None)
+    coefs, residuals, rank, sing = linalg.lstsq(A, b, rcond=None)
 
     pc = coefs, borders
 
-    xs_len = xs.max()-xs.min()
-    new_xs = np.linspace(xs.min()-xs_len/2,xs.max()+xs_len/2,100)
+    xs_len = xs.max() - xs.min()
+    new_xs = np.linspace(xs.min() - xs_len/2, xs.max() + xs_len/2, 100)
 
     Ys1 = piecewisecubic(pc, new_xs, verbose=True) # Cubic extrapolation is default
     Ys2 = piecewisecubic(pc, new_xs, extrapolation="linear", verbose=True)
     Ys3 = piecewisecubic(pc, new_xs, extrapolation="constant", verbose=True)
 
-    plt.plot(xs,ys,c='black',linewidth=2.5)
-    plt.plot(new_xs,Ys1,c='r')
-    plt.plot(new_xs,Ys2,c='g')
-    plt.plot(new_xs,Ys3,c='b')
+    plt.plot(xs, ys, c='black', linewidth=2.5)
+    plt.plot(new_xs, Ys1, c='r')
+    plt.plot(new_xs, Ys2, c='g')
+    plt.plot(new_xs, Ys3, c='b')
     plt.show()
