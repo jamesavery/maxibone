26fe9b4974e94ea88f745954adb04dae1fb05abb
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-16 15:04:26 +0200

    #34 Added docstrings to the diffusion python bindings.

--- src/pybind/diffusion-pybind.cc
+++ src/pybind/diffusion-pybind.cc
@@ -1,7 +1,28 @@
+/**
+ * @file diffusion-pybind.cc
+ * @author Carl-Johannes Johnsen (carl-johannes@di.ku.dk)
+ * @brief Python bindings for 3D Diffusion approximation using repeated applications of a 1D gaussian in each dimension.
+ * @version 0.1
+ * @date 2024-09-16
+ *
+ * @copyright Copyright (c) 2024
+ */
 #include "diffusion.cc"
 
 namespace python_api {
 
+    /**
+     * In-memory 3D diffusion approximation using repeated applications of a 1D gaussian in each dimension.
+     * This version assumes that the host has enough memory to hold both input, output and working memory.
+     * This translates to `N.z*N.y*N.x * (sizeof(uint8_t) + 2*sizeof(float) + sizeof(uint16_t)) + kernel_size`.
+     * If the GPU implementation is chosen and the  total size of the input, output and working memory exceeds the memory available on the GPU device, the out-of-core implementation is chosen.
+     * Otherwise, the in-memory implementation is chosen.
+     *
+     * @param np_voxels The input mask.
+     * @param np_kernel The 1D gaussian kernel to apply.
+     * @param np_output The output array.
+     * @param repititions The number of repititions to apply the kernel.
+     */
     void diffusion_in_memory(np_array<uint8_t> &np_voxels, const np_array<float> &np_kernel, np_array<uint16_t> &np_output, const int64_t repititions = 1) {
         auto
             voxels_info = np_voxels.request(),
@@ -31,6 +52,18 @@ namespace python_api {
 
     }
 
+    /**
+     * On-disk 3D diffusion approximation using repeated applications of a 1D gaussian in each dimension.
+     * This version reads and writes the results to disk, as they are assumed too large to fit in memory.
+     *
+     * @param input_file The path to the input file.
+     * @param np_kernel The 1D gaussian kernel to apply.
+     * @param output_file The path to the output file.
+     * @param py_total_shape The shape of the total volume.
+     * @param py_global_shape The shape of each chunk to load in and out.
+     * @param repititions The number of repititions to apply the kernel.
+     * @param verbose Whether debug information should be printed.
+     */
     void diffusion_on_disk(const std::string &input_file, const np_array<float> &np_kernel, const std::string &output_file, const std::tuple<int64_t, int64_t, int64_t> &py_total_shape, const std::tuple<int64_t, int64_t, int64_t> &py_global_shape, const int64_t repititions, const bool verbose = false) {
         shape_t
             total_shape = {std::get<0>(py_total_shape), std::get<1>(py_total_shape), std::get<2>(py_total_shape)},
