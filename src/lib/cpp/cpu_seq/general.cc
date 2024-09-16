1139ca4286440db5baf9ec7cb427fc9d6169dd88
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-16 15:20:20 +0200

    #34 Added file docstrings to the general implementations.

--- src/lib/cpp/cpu_seq/general.cc
+++ src/lib/cpp/cpu_seq/general.cc
@@ -1,3 +1,12 @@
+/**
+ * @file general.cc
+ * @author Carl-Johannes Johnsen (carl-johannes@di.ku.dk)
+ * @brief Generic functions that can be used in a variety of contexts. Mostly parallel implementations of common numpy / scipy functions.
+ * @version 0.1
+ * @date 2024-09-16
+ *
+ * @copyright Copyright (c) 2024
+ */
 #include "general.hh"
 
 // Header only library so far.
