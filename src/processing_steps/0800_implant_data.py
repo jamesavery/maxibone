27fc9ca2c521917c6f090fd45707463296677f5f
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-17 17:47:33 +0200

    #12 General cleanup in 0800

--- src/processing_steps/0800_implant_data.py
+++ src/processing_steps/0800_implant_data.py
@@ -10,114 +10,109 @@ data consists of the following:
 '''
 import matplotlib
 matplotlib.use('Agg')
-import h5py, sys, os, os.path, pathlib, numpy as np, numpy.linalg as la, tqdm
+import sys
 sys.path.append(sys.path[0]+"/../")
 from config.constants import *
-from config.paths import hdf5_root, binary_root
+from config.paths import hdf5_root
+import h5py
 from lib.cpp.gpu.geometry import fill_implant_mask_pre, fill_implant_mask, compute_front_mask
+from lib.py.helpers import commandline_args, plot_middle_planes, update_hdf5, update_hdf5_mask
 import matplotlib.pyplot as plt
-import scipy as sp, scipy.ndimage as ndi, scipy.interpolate as interpolate, scipy.signal as signal
-import vedo, vedo.pointcloud as pc
-from lib.py.helpers import commandline_args, update_hdf5, update_hdf5_mask
-from numpy import array, newaxis as NA
+import numpy as np
+import os
+import os.path
+import tqdm
 
-sample, scale, block_size, verbose = commandline_args({"sample" : "<required>",
-                                           "scale" : 2,
-                                           "block_size" : 256,
-                                           "verbose" : 1})
+if __name__ == "__main__":
+    sample, scale, block_size, verbose = commandline_args({
+        "sample" : "<required>",
+        "scale" : 2,
+        "block_size" : 256,
+        "verbose" : 1
+    })
 
-output_image_dir = f"{hdf5_root}/processed/implant-data/{sample}"
-os.makedirs(output_image_dir, exist_ok=True)
+    output_image_dir = f"{hdf5_root}/processed/implant-data/{sample}"
+    os.makedirs(output_image_dir, exist_ok=True)
 
-if verbose >= 1: print(f"Loading principal axis and cylinder frame-of-references")
-h5meta = h5py.File(f"{hdf5_root}/hdf5-byte/msb/{sample}.h5","r")
-try:
-    h5g = h5meta["implant-FoR"]
-    Muvwp = h5g["UVWp_transform"][:] # Actually all that's needed
-    bbox = h5g["bounding_box_UVWp"][:]
-    implant_radius = h5g.attrs["implant_radius"]
-    theta_range    = h5g["theta_range"][:]
-    h5meta.close()
-except Exception as e:
-    print(f"Cant't read implant frame-of-reference: {e}")
-    print(f"Make sure you have run implant-FoR.py for {sample} at scale {scale}x")
-    sys.exit(-1)
+    if verbose >= 1: print(f"Loading principal axis and cylinder frame-of-references")
+    h5meta = h5py.File(f"{hdf5_root}/hdf5-byte/msb/{sample}.h5","r")
+    try:
+        h5g = h5meta["implant-FoR"]
+        Muvwp = h5g["UVWp_transform"][:] # Actually all that's needed
+        bbox = h5g["bounding_box_UVWp"][:]
+        implant_radius = h5g.attrs["implant_radius"]
+        theta_range = h5g["theta_range"][:]
+        h5meta.close()
+    except Exception as e:
+        print(f"Cant't read implant frame-of-reference: {e}")
+        print(f"Make sure you have run implant-FoR.py for {sample} at scale {scale}x")
+        sys.exit(-1)
 
-if verbose >= 1: print(f"Loading {scale}x implant mask from {hdf5_root}/masks/{scale}x/{sample}.h5")
-try:
-    implant_file = h5py.File(f"{hdf5_root}/masks/{scale}x/{sample}.h5",'r')
-    implant      = implant_file["implant/mask"]
-    voxel_size   = implant_file["implant"].attrs["voxel_size"]
-    nz,ny,nx     = implant.shape
-except Exception as e:
-    print(f"Can't read implant mask {e}.\nDid you run segment-implant-cc.py?")
-    sys.exit(-1)
+    if verbose >= 1: print(f"Loading {scale}x implant mask from {hdf5_root}/masks/{scale}x/{sample}.h5")
+    try:
+        implant_file = h5py.File(f"{hdf5_root}/masks/{scale}x/{sample}.h5",'r')
+        implant      = implant_file["implant/mask"]
+        voxel_size   = implant_file["implant"].attrs["voxel_size"]
+        nz,ny,nx     = implant.shape
+    except Exception as e:
+        print(f"Can't read implant mask {e}.\nDid you run segment-implant-cc.py?")
+        sys.exit(-1)
 
-((Up_min,Up_max),(Vp_min,Vp_max),(Wp_min,Wp_max)) = bbox
+    ((Up_min, Up_max), (Vp_min, Vp_max), (Wp_min, Wp_max)) = bbox
 
-n_bins = 2048//scale
-rsqr_fraction = 0.7 # Fill in whenever W>Wmin and r<rsqr_fraction*rsqr_maxs[U_i]
-solid_implant_mask = np.empty(implant.shape, dtype=np.uint8)
-rsqr_maxs          = np.zeros((n_bins,), dtype=np.float32)
-profile            = np.zeros((n_bins,), dtype=np.float32)
+    n_bins = 2048//scale
+    rsqr_fraction = 0.7 # Fill in whenever W>Wmin and r<rsqr_fraction*rsqr_maxs[U_i]
+    solid_implant_mask = np.empty(implant.shape, dtype=np.uint8)
+    rsqr_maxs          = np.zeros((n_bins,), dtype=np.float32)
+    profile            = np.zeros((n_bins,), dtype=np.float32)
 
-bbox_flat  = tuple(bbox.flatten())
-Muvwp_flat = tuple(Muvwp.flatten())
-#if verbose >= 1: print(f"Filling implant mask")
-n_blocks = (nz + block_size-1) // block_size
-mask = np.zeros((block_size,ny,nx), np.uint8)
-thetas = np.zeros((2,), np.float32)
+    bbox_flat  = tuple(bbox.flatten())
+    Muvwp_flat = tuple(Muvwp.flatten())
+    #if verbose >= 1: print(f"Filling implant mask")
+    n_blocks = (nz + block_size-1) // block_size
+    mask = np.zeros((block_size,ny,nx), np.uint8)
+    thetas = np.zeros((2,), np.float32)
 
-for i in tqdm.tqdm(range(n_blocks), desc="Filling implant mask pre"):
-    z0 = i*block_size
-    z1 = (i+1)*block_size
-    if z1 > nz: z1 = nz
-    mask[:z1-z0,:,:] = implant[z0:z1,:,:].astype(np.uint8)
-    fill_implant_mask_pre(mask[:z1-z0],
-                        z0*ny*nx, voxel_size, bbox_flat,
-                        Muvwp_flat, thetas, rsqr_maxs)
+    for i in tqdm.tqdm(range(n_blocks), desc="Filling implant mask pre"):
+        z0 = i * block_size
+        z1 = (i+1) * block_size
+        if z1 > nz: z1 = nz
+        mask[:z1-z0,:,:] = implant[z0:z1,:,:].astype(np.uint8)
+        fill_implant_mask_pre(mask[:z1-z0], z0*ny*nx, voxel_size, bbox_flat, Muvwp_flat, thetas, rsqr_maxs)
 
-for i in tqdm.tqdm(range(n_blocks), desc="Filling implant mask"):
-    z0 = i*block_size
-    z1 = (i+1)*block_size
-    if z1 > nz: z1 = nz
-    mask[:z1-z0,:,:] = implant[z0:z1,:,:].astype(np.uint8)
-    fill_implant_mask(mask[:z1-z0],
-                        z0*ny*nx, voxel_size, bbox_flat, rsqr_fraction,
-                        Muvwp_flat, thetas, rsqr_maxs, solid_implant_mask, profile)
+    for i in tqdm.tqdm(range(n_blocks), desc="Filling implant mask"):
+        z0 = i*block_size
+        z1 = (i+1)*block_size
+        if z1 > nz: z1 = nz
+        mask[:z1-z0,:,:] = implant[z0:z1,:,:].astype(np.uint8)
+        fill_implant_mask(mask[:z1-z0], z0*ny*nx, voxel_size, bbox_flat, rsqr_fraction, Muvwp_flat, thetas, rsqr_maxs, solid_implant_mask, profile)
 
-implant_file.close()
+    implant_file.close()
 
-update_hdf5(f"{hdf5_root}/hdf5-byte/msb/{sample}.h5",
-            group_name="implant-data",
-            datasets  = {"quarter_profile":profile,
-                         "r_maxs": np.sqrt(rsqr_maxs)}
-)
-plt.plot(profile)
-plt.savefig(f"{output_image_dir}/profile.png", bbox_inches='tight')
-plt.clf()
+    update_hdf5(f"{hdf5_root}/hdf5-byte/msb/{sample}.h5",
+                group_name="implant-data",
+                datasets  = {"quarter_profile": profile,
+                            "r_maxs": np.sqrt(rsqr_maxs)}
+    )
+    plt.plot(profile)
+    plt.savefig(f"{output_image_dir}/profile.png", bbox_inches='tight')
+    plt.clf()
 
-update_hdf5_mask(f"{hdf5_root}/masks/{scale}x/{sample}.h5",
-                 group_name="implant_solid",
-                 datasets={"mask":solid_implant_mask.astype(bool,copy=False)},
-                 attributes={"sample":sample,"scale":scale,"voxel_size":voxel_size})
-plt.imshow(solid_implant_mask[solid_implant_mask.shape[0]//2, :, :]); plt.savefig(f"{output_image_dir}/solid_implant_mask_yx.png", bbox_inches='tight')
-plt.imshow(solid_implant_mask[:, solid_implant_mask.shape[1]//2, :]); plt.savefig(f"{output_image_dir}/solid_implant_mask_zx.png", bbox_inches='tight')
-plt.imshow(solid_implant_mask[:, :, solid_implant_mask.shape[2]//2]); plt.savefig(f"{output_image_dir}/solid_implant_mask_zy.png", bbox_inches='tight')
+    update_hdf5_mask(f"{hdf5_root}/masks/{scale}x/{sample}.h5",
+                    group_name="implant_solid",
+                    datasets={"mask": solid_implant_mask.astype(bool, copy=False)},
+                    attributes={"sample": sample, "scale": scale, "voxel_size": voxel_size})
+    plot_middle_planes(solid_implant_mask, output_image_dir, "solid_implant_mask")
 
-# Compute front mask
-print ("Computing front mask")
-front_mask = np.zeros_like(solid_implant_mask)
-compute_front_mask(solid_implant_mask, voxel_size,
-                   Muvwp_flat, bbox_flat, front_mask)
+    # Compute front mask
+    print ("Computing front mask")
+    front_mask = np.zeros_like(solid_implant_mask)
+    compute_front_mask(solid_implant_mask, voxel_size, Muvwp_flat, bbox_flat, front_mask)
 
-del solid_implant_mask
+    del solid_implant_mask
 
-update_hdf5_mask(f"{hdf5_root}/masks/{scale}x/{sample}.h5",
-                 group_name="cut_cylinder_bone",
-                 datasets={"mask":front_mask.astype(bool,copy=False)},
-                 attributes={"sample":sample,"scale":scale,"voxel_size":voxel_size})
-plt.imshow(front_mask[front_mask.shape[0]//2, :, :]); plt.savefig(f"{output_image_dir}/front_mask_yx.png", bbox_inches='tight')
-plt.imshow(front_mask[:, front_mask.shape[1]//2, :]); plt.savefig(f"{output_image_dir}/front_mask_zx.png", bbox_inches='tight')
-plt.imshow(front_mask[:, :, front_mask.shape[2]//2]); plt.savefig(f"{output_image_dir}/front_mask_zy.png", bbox_inches='tight')
-
+    update_hdf5_mask(f"{hdf5_root}/masks/{scale}x/{sample}.h5",
+                    group_name="cut_cylinder_bone",
+                    datasets={"mask":front_mask.astype(bool,copy=False)},
+                    attributes={"sample":sample,"scale":scale,"voxel_size":voxel_size})
+    plot_middle_planes(front_mask, output_image_dir, "front_mask")
