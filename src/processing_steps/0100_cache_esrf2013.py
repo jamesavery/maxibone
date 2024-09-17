20cd17e82be8fd8507903e8c985087b284d8355a
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-17 12:44:33 +0200

    #12 General cleanup in 0100

--- src/processing_steps/0100_cache_esrf2013.py
+++ src/processing_steps/0100_cache_esrf2013.py
@@ -9,33 +9,41 @@ from lib.py.helpers import commandline_args
 from config.paths import esrf_data_sftp, esrf_data_local
 
 if __name__ == "__main__":
-    sample, experiment, verbose = commandline_args({"sample" : "<required>",
-                                                    "experiment" : "esrf_dental_implants_april_2013",
-                                                    "verbose" : 1})
-    
-    index_dir  = f"{esrf_data_local}/{experiment}/index/";
+    sample, experiment, verbose = commandline_args({
+        "sample" : "<required>",
+        "experiment" : "esrf_dental_implants_april_2013",
+        "verbose" : 1
+    })
+
+    # Read the index file for the sample
+    index_dir  = f"{esrf_data_local}/{experiment}/index/"
     with open(f"{index_dir}/{sample}.txt") as f:
         volume_xmls = f.readlines()
 
-    run_dir = os.getcwd()
+    # Download the contents of each volume
     for volume_xml in volume_xmls:
         volume_dir = os.path.dirname(volume_xml)
         local_directory = f"{esrf_data_local}/{experiment}/{volume_dir}"
         sftp_directory  = f"{esrf_data_sftp}/{experiment}/{volume_dir}"
+
         if verbose >= 1: print(f"Local: Creating directory {local_directory}")
         pathlib.Path(local_directory).mkdir(parents=True, exist_ok=True)
 
         with fabric.Connection('erda') as connection:
             if verbose >= 1: print("Connected to ERDA")
+
             with connection.sftp() as sftp:
                 if verbose >= 1: print(f"SFTP: Attempting to chdir to {sftp_directory}")
                 sftp.chdir(sftp_directory)
+
                 if verbose >= 1: print(f"SFTP: Reading directory contents")
                 files = sftp.listdir()
-                if verbose >= 1: print(f"Local: Attempting to chdir to {local_directory}")                
+
+                if verbose >= 1: print(f"Local: Attempting to chdir to {local_directory}")
                 os.chdir(local_directory)
+
                 if verbose >= 1: print("SFTP: Downloading subvolume contents")
                 for f in tqdm.tqdm(files):
-                    sftp.get(f,f)
+                    sftp.get(f, f)
 
             connection.close()
