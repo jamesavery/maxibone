5f23574411d0d5ce18b7b4136ceb3cc70b42e6fd
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-17 10:31:12 +0200

    #34 Added docstrings to histograms-pybind

--- src/pybind/histograms-pybind.cc
+++ src/pybind/histograms-pybind.cc
@@ -1,12 +1,32 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/numpy.h>
-
-namespace py = pybind11;
-
+/**
+ * @file histograms-pybind.cc
+ * @author Carl-Johannes Johnsen (carl-johannes@di.ku.dk)
+ * @brief Python bindings for the 2D histogram functions.
+ * @version 0.1
+ * @date 2024-09-17
+ *
+ * @copyright Copyright (c) 2024
+ */
 #include "histograms.cc"
 
 namespace python_api {
 
+    /**
+     * Computes four 2D histograms of the voxels.
+     * A 2D histogram is where each row is a histogram of the voxels in a slice of the 3D volume.
+     * E.g. for the x-axis, a histogram is computed for each slice in the x-direction.
+     * Assumes that the `*_bins` are pre-allocated and zeroed.
+     *
+     * @param np_voxels The 3D volume of voxels.
+     * @param np_offset The global offset of the block. Used for out-of-core processing.
+     * @param np_x_bins The output array for the x-axis histograms.
+     * @param np_y_bins The output array for the y-axis histograms.
+     * @param np_z_bins The output array for the z-axis histograms.
+     * @param np_r_bins The output array for the radial histograms.
+     * @param center The YX center of the volume. Used for computing the radial position of each voxel for the radial histogram.
+     * @param vrange The value range of the voxels.
+     * @param verbose Whether to print debug information.
+     */
     void axis_histograms(const np_array<voxel_type> np_voxels,
                          const std::tuple<int64_t,int64_t,int64_t> np_offset,
                          np_array<uint64_t> &np_x_bins,
@@ -45,6 +65,19 @@ namespace python_api {
                            voxel_bins, Nr, center, vrange, verbose);
     }
 
+    /**
+     * Computes a 2D field histogram.
+     * A field histogram is where for each `v = voxel[z,y,x]` value and corresponding `f = field[z,y,x]` value, index `i,j` in the resulting histogram is incremented, where `i` is the field bin, and `j` is the voxel bin.
+     * If the field shape is less than the voxel shape, the field is upsampled to the voxel shape.
+     *
+     * @param np_voxels The 3D volume of voxels.
+     * @param np_field The 3D volume of fields.
+     * @param np_offset The global offset of the block. Used for out-of-core processing.
+     * @param np_bins The output array for the histogram.
+     * @param vrange The value range of the voxels.
+     * @param frange The value range of the fields.
+     * @param verbose Whether to print debug information.
+     */
     void field_histogram(const np_array<voxel_type> &np_voxels,
                          const np_array<field_type> &np_field,
                          const std::tuple<int64_t,int64_t,int64_t> np_offset,
@@ -73,7 +106,20 @@ namespace python_api {
         NS::field_histogram(voxels, field, voxels_shape, field_shape, offset, voxels_shape, bins, voxel_bins, field_bins, vrange, frange, verbose);
     }
 
-    
+    /**
+     * Computes a 2D field histogram.
+     * A field histogram is where for each `v = voxel[z,y,x]` value and corresponding `f = field[z,y,x]` value, index `i,j` in the resulting histogram is incremented, where `i` is the field bin, and `j` is the voxel bin.
+     * This function differs from `field_histogram` in that it resamples the field to the voxel shape using the neighbouring field values, rather than just choosing the closest.
+     *
+     * @param np_voxels The 3D volume of voxels.
+     * @param np_field The 3D volume of fields.
+     * @param np_offset The global offset of the block. Used for out-of-core processing.
+     * @param np_block_size The size of the block. Used for out-of-core processing.
+     * @param np_bins The output array for the histogram.
+     * @param vrange The value range of the voxels.
+     * @param frange The value range of the fields.
+     * @param verbose Whether to print debug information.
+     */
     void field_histogram_resample(const np_array<voxel_type> np_voxels,
                         const np_array<field_type> np_field,
                         const std::tuple<int64_t,int64_t,int64_t> np_offset,
