986704def216114ce88adb3a25e0f7a9a9fe96a1
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-18 05:38:48 +0200

    #12 General cleanup in 0900

--- src/processing_steps/0900_generate_gauss_c.py
+++ src/processing_steps/0900_generate_gauss_c.py
@@ -3,60 +3,62 @@
 Diffusion approximation by repeated Gaussian blurs.
 This provides an estimate to counter the "glow" of the implant in the tomogram.
 '''
+import sys
+sys.path.append(sys.path[0]+"/../")
 import matplotlib
 matplotlib.use('Agg')
-import sys, pathlib, datetime, numpy as np, h5py, timeit, matplotlib.pyplot as plt, edt
-sys.path.append(sys.path[0]+"/../")
-from matplotlib import image
-from PIL import Image
-from tqdm import tqdm
-from math import pi, sqrt, exp
-from scipy import ndimage as ndi
-import multiprocessing as mp
 from config.paths import hdf5_root, binary_root
-from lib.py.helpers import commandline_args, gaussian_kernel, to_int
+import h5py
 from lib.cpp.gpu.diffusion import diffusion
 from lib.cpp.cpu.io import load_slice, write_slice
-NA = np.newaxis
+from lib.py.helpers import commandline_args, gauss_kernel, generate_cylinder_mask, plot_middle_planes, to_int
+import numpy as np
+import matplotlib.pyplot as plt
+import pathlib
+from PIL import Image
 import psutil
-
-internal_type = np.float32
-result_type = np.uint16
+import timeit
+from tqdm import tqdm
+from scipy import ndimage as ndi
 
 # sigma is given in physical units, i.e. in micrometers, in order to give scale-invariant results.
 if __name__ == '__main__':
-    sample, scale, sigma, reps, verify, verbose = \
-        commandline_args({
-            "sample" : "<required>",
-            "scale" : 1,
-            "sigma" : 10.0,
-            "repititions" : 42,
-            "verify_against_ndimage" : False,
-            "verbose" : 2
-        })
+    sample, scale, sigma, reps, verify, verbose = commandline_args({
+        "sample" : "<required>",
+        "scale" : 1,
+        "sigma" : 10.0,
+        "repititions" : 42,
+        "verify_against_ndimage" : False,
+        "verbose" : 2
+    })
     if verbose >= 1: print(f"Diffusion approximation by repeated Gaussian blurs.\n")
+
+    # The internal type is the type used for the computation, the result type is the type used for the output.
+    internal_type = np.float32
+    result_type = np.uint16
 
     output_dir = f"{binary_root}/fields/implant-gauss/{scale}x"
     pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+    output_image_dir = f"{hdf5_root}/processed/field-gauss/{scale}x/{sample}"
+    if verbose >= 2:
+        pathlib.Path(output_image_dir).mkdir(parents=True, exist_ok=True)
 
     if verbose >= 1: print(f"Loading implant_solid mask from {hdf5_root}/masks/{scale}x/{sample}.h5")
     with h5py.File(f"{hdf5_root}/masks/{scale}x/{sample}.h5","r") as f:
         implant_solid = f['implant_solid/mask']
-        voxel_size   = f["implant"].attrs["voxel_size"]
+        voxel_size = f["implant"].attrs["voxel_size"]
         nz,ny,nx = implant_solid.shape
         implant_mask = implant_solid[:]
 
     sigma_voxels = sigma / voxel_size
-    if verbose >= 1: print(f"At scale {scale}x, voxel size is {voxel_size} micrometers.")
-    if verbose >= 1: print(f"Using sigma={sigma} micrometers, sigma_voxels={sigma_voxels}.")
 
-    if verbose >= 1: print(f"Implant mask has shape {(nz,ny,nx)}")
+    if verbose >= 1:
+        print(f"At scale {scale}x, voxel size is {voxel_size} micrometers.")
+        print(f"Using sigma={sigma} micrometers, sigma_voxels={sigma_voxels}.")
+        print(f"Implant mask has shape {(nz,ny,nx)}")
 
     if verbose >= 2:
-        print(f"Writing PNGs of implant mask slices to {output_dir}")
-        Image.fromarray(to_int(implant_mask[:,:,nx//2].astype(internal_type),np.uint8)).save(f"{output_dir}/{sample}-mask-yz.png")
-        Image.fromarray(to_int(implant_mask[:,ny//2,:].astype(internal_type),np.uint8)).save(f"{output_dir}/{sample}-mask-xz.png")
-        Image.fromarray(to_int(implant_mask[nz//2,:,:].astype(internal_type),np.uint8)).save(f"{output_dir}/{sample}-mask-xy.png")
+        plot_middle_planes(implant_mask, output_image_dir, f'{sample}-mask')
 
     kernel = gauss_kernel(sigma_voxels)
 
@@ -98,35 +100,29 @@ if __name__ == '__main__':
         result = np.empty((nz,ny,nx), dtype=result_type)
         load_slice(result, output_path, (0,0,0), (nz,ny,nx))
     else:
-        result = np.zeros(implant_mask.shape,dtype=result_type)
+        result = np.zeros(implant_mask.shape, dtype=result_type)
         if verbose >= 1:
             start = timeit.default_timer()
+            print(f"Repeated Gauss blurs ({reps} iterations, sigma_voxels={sigma_voxels}, kernel length={kernel.shape} coefficients)")
 
-        if verbose >= 1: print(f"Repeated Gauss blurs ({reps} iterations, sigma_voxels={sigma_voxels}, kernel length={kernel.shape} coefficients)")
         diffusion(implant_mask, kernel, result, reps)
+
         if verbose >= 1:
             diffusion_time = timeit.default_timer() - start
             print (f'C++ edition took {diffusion_time:.02f} seconds')
 
-    xs = np.linspace(-1,1,nx)
-    rs = np.sqrt(xs[NA,NA,:]**2 + xs[NA,:,NA]**2)
-    cylinder_mask = (rs<=1)
+    cylinder_mask = generate_cylinder_mask(nx)
 
     if verbose >= 2:
-        print(f"Debug: Writing PNGs of result slices to {output_dir}")
-        Image.fromarray(to_int(result[nz//2,:,:],np.uint8)).save(f'{output_dir}/{sample}-gauss-xy.png')
-        Image.fromarray(to_int(result[:,ny//2,:],np.uint8)).save(f'{output_dir}/{sample}-gauss-xz.png')
-        Image.fromarray(to_int(result[:,:,nx//2],np.uint8)).save(f'{output_dir}/{sample}-gauss-yz.png')
-        Image.fromarray(to_int((np.abs(result[nz//2,:,:])!=0).astype(np.uint8),np.uint8)).save(f'{output_dir}/{sample}-gauss-xy-nonzero.png')
-        Image.fromarray(to_int((np.abs(result[:,ny//2,:])!=0).astype(np.uint8),np.uint8)).save(f'{output_dir}/{sample}-gauss-xz-nonzero.png')
-        Image.fromarray(to_int((np.abs(result[:,:,nx//2])!=0).astype(np.uint8),np.uint8)).save(f'{output_dir}/{sample}-gauss-yz-nonzero.png')
+        plot_middle_planes(result, output_image_dir, f'{sample}-gauss')
+        plot_middle_planes(result, output_image_dir, f'{sample}-gauss-nonzero', lambda x: (np.abs(x) != 0).astype(np.uint8))
 
     if verbose >= 1: print(f"Writing diffusion-field to {output_dir}/{sample}.npy")
     np.save(f'{output_dir}/{sample}.npy', result*cylinder_mask)
 
     if verify and scale > 1: # generate ndimage comparison, but only for scale > 1
         start = timeit.default_timer()
-        control = (implant_mask>0).astype(internal_type)
+        control = (implant_mask > 0).astype(internal_type)
         for _ in tqdm(range(reps), desc='ndimage repititions'):
             control[:] = ndi.gaussian_filter(control, sigma_voxels, mode='constant', cval=0)
             control[implant_mask] = 1 # Illuminate
@@ -135,28 +131,25 @@ if __name__ == '__main__':
         print (f'ndimage edition took {ndimage_time:.02f} seconds')
         print (f'C++ edition is {ndimage_time/diffusion_time:.02f} times faster')
         np.save(f'{output_dir}/{sample}_ndimage.npy',control)
+
         if verbose >= 2:
-            Image.fromarray(to_int(control[nz//2,:,:],np.uint8)).save(f'{output_dir}/{sample}-control-xy.png')
-            Image.fromarray(to_int(control[:,ny//2,:],np.uint8)).save(f'{output_dir}/{sample}-control-xz.png')
-            Image.fromarray(to_int(control[:,:,nx//2],np.uint8)).save(f'{output_dir}/{sample}-control-yz.png')
+            plot_middle_planes(control, output_image_dir, f'{sample}-control')
+
         if result_type == np.uint8 or result_type == np.uint16:
             diff = result.astype(np.int32) - control.astype(np.int32)
         else:
             diff = result - control
         diff_abs = np.abs(diff)
+
+        if verbose >= 2:
+            plot_middle_planes(diff, output_image_dir, f'{sample}-diff')
+            plot_middle_planes(diff_abs, output_image_dir, f'{sample}-diff-abs')
+
         diff_sum = diff_abs.sum()
         diff_max = diff_abs.max()
         diff_mean = diff_abs.mean()
+
         print (f'Total difference: {diff_sum}')
         print (f'Max abs difference: {diff_max}')
         print (f'Min and max difference: {diff.min()} {diff.max()}')
         print (f'Mean difference: {diff_mean}')
-        if diff_max > 1e-7:
-            for name, diff_img in [
-                    ('xy', diff[nz//2,:,:]),
-                    ('xz', diff[:,ny//2,:]),
-                    ('yz', diff[:,:,nx//2])]:
-                plt.figure(figsize=(20,20))
-                plt.imshow(diff_img)
-                plt.colorbar()
-                plt.savefig(f'{output_dir}/{sample}-diff-{name}.png', bbox_inches='tight')
