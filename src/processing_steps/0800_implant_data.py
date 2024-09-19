655e71e04b335d45da501e3162bb63cca88e52d9
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-19 13:15:56 +0200

    #33 Updated 0800 to use argparse

--- src/processing_steps/0800_implant_data.py
+++ src/processing_steps/0800_implant_data.py
@@ -17,7 +17,8 @@ from config.constants import *
 from config.paths import hdf5_root
 import h5py
 from lib.cpp.gpu.geometry import fill_implant_mask_pre, fill_implant_mask, compute_front_mask
-from lib.py.helpers import commandline_args, plot_middle_planes, update_hdf5, update_hdf5_mask
+from lib.py.commandline_args import default_parser
+from lib.py.helpers import plot_middle_planes, update_hdf5, update_hdf5_mask
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -25,18 +26,13 @@ import os.path
 import tqdm
 
 if __name__ == "__main__":
-    sample, scale, block_size, verbose = commandline_args({
-        "sample" : "<required>",
-        "scale" : 2,
-        "block_size" : 256,
-        "verbose" : 1
-    })
+    args = default_parser(__doc__).parse_args()
 
-    output_image_dir = f"{hdf5_root}/processed/implant-data/{sample}"
+    output_image_dir = f"{hdf5_root}/processed/implant-data/{args.sample}"
     os.makedirs(output_image_dir, exist_ok=True)
 
-    if verbose >= 1: print(f"Loading principal axis and cylinder frame-of-references")
-    h5meta = h5py.File(f"{hdf5_root}/hdf5-byte/msb/{sample}.h5","r")
+    if args.verbose >= 1: print(f"Loading principal axis and cylinder frame-of-references")
+    h5meta = h5py.File(f"{hdf5_root}/hdf5-byte/msb/{args.sample}.h5","r")
     try:
         h5g = h5meta["implant-FoR"]
         Muvwp = h5g["UVWp_transform"][:] # Actually all that's needed
@@ -46,12 +42,12 @@ if __name__ == "__main__":
         h5meta.close()
     except Exception as e:
         print(f"Cant't read implant frame-of-reference: {e}")
-        print(f"Make sure you have run implant-FoR.py for {sample} at scale {scale}x")
+        print(f"Make sure you have run implant-FoR.py for {args.sample} at scale {args.sample_scale}x")
         sys.exit(-1)
 
-    if verbose >= 1: print(f"Loading {scale}x implant mask from {hdf5_root}/masks/{scale}x/{sample}.h5")
+    if args.verbose >= 1: print(f"Loading {args.sample_scale}x implant mask from {hdf5_root}/masks/{args.sample_scale}x/{args.sample}.h5")
     try:
-        implant_file = h5py.File(f"{hdf5_root}/masks/{scale}x/{sample}.h5",'r')
+        implant_file = h5py.File(f"{hdf5_root}/masks/{args.sample_scale}x/{args.sample}.h5",'r')
         implant      = implant_file["implant/mask"]
         voxel_size   = implant_file["implant"].attrs["voxel_size"]
         nz,ny,nx     = implant.shape
@@ -61,7 +57,7 @@ if __name__ == "__main__":
 
     ((Up_min, Up_max), (Vp_min, Vp_max), (Wp_min, Wp_max)) = bbox
 
-    n_bins = 2048//scale
+    n_bins = 2048//args.sample_scale
     rsqr_fraction = 0.7 # Fill in whenever W>Wmin and r<rsqr_fraction*rsqr_maxs[U_i]
     solid_implant_mask = np.empty(implant.shape, dtype=np.uint8)
     rsqr_maxs          = np.zeros((n_bins,), dtype=np.float32)
@@ -70,27 +66,27 @@ if __name__ == "__main__":
     bbox_flat  = tuple(bbox.flatten())
     Muvwp_flat = tuple(Muvwp.flatten())
     #if verbose >= 1: print(f"Filling implant mask")
-    n_blocks = (nz + block_size-1) // block_size
-    mask = np.zeros((block_size,ny,nx), np.uint8)
+    n_blocks = (nz + args.chunk_size-1) // args.chunk_size
+    mask = np.zeros((args.chunk_size,ny,nx), np.uint8)
     thetas = np.zeros((2,), np.float32)
 
     for i in tqdm.tqdm(range(n_blocks), desc="Filling implant mask pre"):
-        z0 = i * block_size
-        z1 = (i+1) * block_size
+        z0 = i * args.chunk_size
+        z1 = (i+1) * args.chunk_size
         if z1 > nz: z1 = nz
         mask[:z1-z0,:,:] = implant[z0:z1,:,:].astype(np.uint8)
         fill_implant_mask_pre(mask[:z1-z0], z0*ny*nx, voxel_size, bbox_flat, Muvwp_flat, thetas, rsqr_maxs)
 
     for i in tqdm.tqdm(range(n_blocks), desc="Filling implant mask"):
-        z0 = i*block_size
-        z1 = (i+1)*block_size
+        z0 = i*args.chunk_size
+        z1 = (i+1)*args.chunk_size
         if z1 > nz: z1 = nz
         mask[:z1-z0,:,:] = implant[z0:z1,:,:].astype(np.uint8)
         fill_implant_mask(mask[:z1-z0], z0*ny*nx, voxel_size, bbox_flat, rsqr_fraction, Muvwp_flat, thetas, rsqr_maxs, solid_implant_mask, profile)
 
     implant_file.close()
 
-    update_hdf5(f"{hdf5_root}/hdf5-byte/msb/{sample}.h5",
+    update_hdf5(f"{hdf5_root}/hdf5-byte/msb/{args.sample}.h5",
                 group_name="implant-data",
                 datasets  = {"quarter_profile": profile,
                             "r_maxs": np.sqrt(rsqr_maxs)}
@@ -99,10 +95,10 @@ if __name__ == "__main__":
     plt.savefig(f"{output_image_dir}/profile.png", bbox_inches='tight')
     plt.clf()
 
-    update_hdf5_mask(f"{hdf5_root}/masks/{scale}x/{sample}.h5",
+    update_hdf5_mask(f"{hdf5_root}/masks/{args.sample_scale}x/{args.sample}.h5",
                     group_name="implant_solid",
                     datasets={"mask": solid_implant_mask.astype(bool, copy=False)},
-                    attributes={"sample": sample, "scale": scale, "voxel_size": voxel_size})
+                    attributes={"sample": args.sample, "scale": args.sample_scale, "voxel_size": voxel_size})
     plot_middle_planes(solid_implant_mask, output_image_dir, "solid_implant_mask")
 
     # Compute front mask
@@ -112,8 +108,8 @@ if __name__ == "__main__":
 
     del solid_implant_mask
 
-    update_hdf5_mask(f"{hdf5_root}/masks/{scale}x/{sample}.h5",
+    update_hdf5_mask(f"{hdf5_root}/masks/{args.sample_scale}x/{args.sample}.h5",
                     group_name="cut_cylinder_bone",
                     datasets={"mask":front_mask.astype(bool,copy=False)},
-                    attributes={"sample":sample,"scale":scale,"voxel_size":voxel_size})
+                    attributes={"sample":args.sample,"scale":args.sample_scale,"voxel_size":voxel_size})
     plot_middle_planes(front_mask, output_image_dir, "front_mask")
