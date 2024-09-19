c66126024e403bf27ec034e769fa4673b676e3db
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-19 08:42:53 +0200

    #33 Started work on base argument parser.

---
+++ src/lib/py/commandline_args.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python3
+'''
+This module is used to parse commandline arguments using the `argparse` module.
+
+It is in its own module, to provide a default yet extendable interface for all of the scripts in this project.
+'''
+import sys
+sys.path.append(sys.path[0]+"/../")
+
+import argparse
+import config.constants as constants
+
+def osteomorph_default_parser(description='MISSING DESCRIPTION', default_scale=1):
+    '''
+    This function returns a default `argparse` parser for all scripts in this project.
+    The caller can then add more arguments to the parser as needed.
+    As a result, the caller must call `parser.parse_args()` to get the arguments.
+
+    Parameters
+    ----------
+    `description` : str
+        A description of the script.
+    `default_scale` : int
+        The default scale of the image to be processed.
+
+    Returns
+    -------
+    `parser` : argparse.ArgumentParser
+        The default parser.
+    '''
+
+    epilog='For more information, please visit github.com/jamesavery/maxibone'
+    parser = argparse.ArgumentParser(description=description, epilog=epilog)
+
+    parser.add_argument('sample', action='store', type=str, required=True,
+        help='The sample name to be processed, e.g. "770c_pag".')
+    parser.add_argument('scale', action='store', type=int, default=default_scale,
+        help=f'The scale of the image to be processed. Default is {default_scale}.')
+    parser.add_argument('-v', '--verbose', action='store', type=int, default=0,
+        help='Set the verbosity level of the script. Default is 0.')
+
+    return parser
