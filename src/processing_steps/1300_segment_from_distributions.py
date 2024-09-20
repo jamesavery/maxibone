d2a88b350803dc8c5efe083ba1ded9b48a76432c
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-20 11:05:07 +0200

    #33 Updated 1300 to use argparse

--- src/processing_steps/1300_segment_from_distributions.py
+++ src/processing_steps/1300_segment_from_distributions.py
@@ -11,13 +11,14 @@ from config.paths import binary_root, hdf5_root as hdf5_root
 import h5py
 from lib.cpp.cpu.io import write_slice
 from lib.cpp.gpu.label import material_prob_justonefieldthx
-from lib.py.helpers import chunk_info, commandline_args, load_chunk, plot_middle_planes
+from lib.py.commandline_args import add_volume, default_parser
+from lib.py.helpers import chunk_info, load_chunk, plot_middle_planes
 import numpy as np
 import matplotlib.pyplot as plt
 import pathlib
 from tqdm import tqdm
 
-def load_probabilities(path, group, axes_names, field_names, m):
+def load_probabilities(path, group, axes_names, field_names, m, verbose):
     '''
     Load the probabilities from the HDF5 file.
 
@@ -55,7 +56,7 @@ def load_probabilities(path, group, axes_names, field_names, m):
 
     return P_axes, P_fields
 
-def load_value_ranges(path, group):
+def load_value_ranges(path, group, verbose):
     '''
     Load the value ranges from the HDF5 file.
 
@@ -103,55 +104,51 @@ def nblocks(size, block_size):
     return (size + block_size - 1) // block_size
 
 if __name__ == '__main__':
-    sample, scale, block_start, block_size, region_mask, group, mask_scale, scheme, field_scale, verbose = commandline_args({
-        'sample' : '<required>',
-        'scale' : 1,
-        "block_start" : 0,
-        "block_size" : 0,
-        'region_mask' :  'bone_region',
-        'group' :  'otsu_separation',
-        'mask_scale' :  8,
-        'scheme' : "gauss+edt", #MIDLERTIDIG
-        'field_scale' : 2,
-        'verbose' : 1
-    })
+    argparser = default_parser(__doc__)
+    argparser = add_volume(argparser, 'field', 2)
+    argparser = add_volume(argparser, 'mask', 8)
+    argparser.add_argument('--chunk-start', action='store', type=int, default=0, nargs='?',
+        help='The starting chunk to process. Default is 0.')
+    argparser.add_argument('--group', action='store', type=str, default='otsu_separation',
+        help='The group in the HDF5 file to load the probabilities from. Default is "otsu_separation".')
+    args = argparser.parse_args()
 
     # TODO scale may not trickle down correctly
     # Iterate over all subvolumes
-    bi = chunk_info(f'{hdf5_root}/hdf5-byte/msb/{sample}.h5', scale, chunk_size=block_size, n_chunks=0, z_offset=block_start)
+    bi = chunk_info(f'{hdf5_root}/hdf5-byte/msb/{args.sample}.h5', args.sample_scale, chunk_size=args.chunk_size, n_chunks=0, z_offset=args.chunk_start)
     Nz, Ny, Nx = bi['dimensions'][:3]
     axes_names =  [] # ["x", "y", "z", "r"] # For later when we also use axes.
-    field_names = [scheme] # TODO: We are currently only using one field.
+    field_names = [args.field] # TODO: We are currently only using one field.
 
-    probs_file = f'{hdf5_root}/processed/probabilities/{sample}.h5'
-    for b in tqdm(range(block_start,block_start+bi['n_chunks']), desc='segmenting subvolumes'):
-        group_name = f"{group}/{region_mask}/"
+    probs_file = f'{hdf5_root}/processed/probabilities/{args.sample}.h5'
+    for b in tqdm(range(args.chunk_start,args.chunk_start+bi['n_chunks']), desc='segmenting subvolumes'):
+        group_name = f"{args.group}/{args.mask}/"
 
-        block_size = bi['chunk_size']
-        zstart = b*block_size
-        zend = min(zstart + block_size, Nz)
+        chunk_size = bi['chunk_size']
+        zstart = b*chunk_size
+        zend = min(zstart + chunk_size, Nz)
 
-        voxels, fields = load_chunk(sample, 1, zstart, block_size, region_mask, mask_scale, field_names, field_scale)
-        (vmin, vmax), (fmin, fmax) = load_value_ranges(probs_file, group_name)
+        voxels, fields = load_chunk(args.sample, 1, zstart, chunk_size, args.mask, args.mask_scale, field_names, args.field_scale)
+        (vmin, vmax), (fmin, fmax) = load_value_ranges(probs_file, group_name, args.verbose)
 
         this_z = zend - zstart
         zmid = this_z // 2
-        if verbose >= 1:
-            plot_dir = f'{hdf5_root}/processed/segmentation/{sample}'
+        if args.verbose >= 1:
+            plot_dir = f'{hdf5_root}/processed/segmentation/{args.sample}'
             pathlib.Path(plot_dir).mkdir(parents=True, exist_ok=True)
             combined_yx = np.zeros((Ny,Nx,3), dtype=np.uint8)
             combined_zy = np.zeros((this_z,Ny,3), dtype=np.uint8)
             combined_zx = np.zeros((this_z,Nx,3), dtype=np.uint8)
 
             plot_middle_planes(voxels, plot_dir, f'{b}_voxels')
-            plot_middle_planes(fields[0], plot_dir, f'{b}_fields_{scheme}')
+            plot_middle_planes(fields[0], plot_dir, f'{b}_fields_{args.field}')
 
         for m in [0,1]:
-            output_dir  = f'{binary_root}/segmented/{scheme}/P{m}/1x/'
-            output_file = f"{output_dir}/{sample}.uint16";
+            output_dir  = f'{binary_root}/segmented/{args.field}/P{m}/1x/'
+            output_file = f"{output_dir}/{args.sample}.uint16";
             pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-            P_axes, P_fields = load_probabilities(probs_file, group_name, axes_names, field_names, m)
+            P_axes, P_fields = load_probabilities(probs_file, group_name, axes_names, field_names, m, args.verbose)
             n_probs = len(P_axes) + len(P_fields)
             result = np.zeros((zend-zstart,Ny,Nx), dtype=np.uint16)
 
@@ -159,21 +156,21 @@ if __name__ == '__main__':
                                                 (vmin,vmax),(fmin,fmax),
                                                 (zstart,0,0), (zend,Ny,Nx));
 
-            if verbose >= 1:
+            if args.verbose >= 1:
                 red = [255,0,0]
                 yellow = [255,255,0]
                 combined_yx[result[zmid,:,:]  > 0] = red if m == 0 else yellow
                 combined_zx[result[:,Ny//2,:] > 0] = red if m == 0 else yellow
                 combined_zy[result[:,:,Nx//2] > 0] = red if m == 0 else yellow
 
-                plot_middle_planes(result, plot_dir, f'{b}_{scheme}_P{m}')
+                plot_middle_planes(result, plot_dir, f'{b}_{args.field}_P{m}')
 
-            if verbose >= 2: print (f'Segmentation has min {result.min()} and max {result.max()}')
+            if args.verbose >= 2: print (f'Segmentation has min {result.min()} and max {result.max()}')
 
-            if verbose >= 1: print(f"Writing results from block {b}")
+            if args.verbose >= 1: print(f"Writing results from block {b}")
             write_slice(result, output_file, (zstart,0,0), result.shape)
 
-        if verbose >= 1:
+        if args.verbose >= 1:
             # Draw two plots in one, one above and one below
             fig = plt.figure(figsize=(34,34*2))
             fig.subplots_adjust(wspace=0, hspace=0)
@@ -182,7 +179,7 @@ if __name__ == '__main__':
             plt.subplot(2,1,2)
             plt.imshow(voxels[zmid,:,:], interpolation='none')
             plt.tight_layout()
-            plt.savefig(f'{plot_dir}/{b}_{scheme}_combined_yx.pdf', bbox_inches='tight')
+            plt.savefig(f'{plot_dir}/{b}_{args.field}_combined_yx.pdf', bbox_inches='tight')
             fig.clear()
             plt.clf()
             plt.cla()
@@ -194,7 +191,7 @@ if __name__ == '__main__':
             plt.subplot(2,1,2)
             plt.imshow(voxels[:,Ny//2,:], interpolation='none')
             plt.tight_layout()
-            plt.savefig(f'{plot_dir}/{b}_{scheme}_combined_zx.pdf', bbox_inches='tight')
+            plt.savefig(f'{plot_dir}/{b}_{args.field}_combined_zx.pdf', bbox_inches='tight')
             fig.clear()
             plt.clf()
             plt.cla()
@@ -206,7 +203,7 @@ if __name__ == '__main__':
             plt.subplot(2,1,2)
             plt.imshow(voxels[:,:,Nx//2], interpolation='none')
             plt.tight_layout()
-            plt.savefig(f'{plot_dir}/{b}_{scheme}_combined_zy.pdf', bbox_inches='tight')
+            plt.savefig(f'{plot_dir}/{b}_{args.field}_combined_zy.pdf', bbox_inches='tight')
             fig.clear()
             plt.clf()
             plt.cla()
