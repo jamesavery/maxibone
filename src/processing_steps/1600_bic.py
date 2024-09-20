45448a4c1b9284bea944bb0b3f9d6597d53c68eb
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-20 11:15:29 +0200

    #33 Updated 1600 to use argparse

--- src/processing_steps/1600_bic.py
+++ src/processing_steps/1600_bic.py
@@ -13,50 +13,46 @@ from config.constants import *
 from config.paths import hdf5_root, binary_root
 import h5py
 from lib.cpp.cpu.analysis import bic
-from lib.py.helpers import commandline_args
+from lib.py.commandline_args import add_volume, default_parser
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
 if __name__ == '__main__':
-    sample, scale, field_scale, threshold,  mask, mask_scale, verbose = commandline_args({
-        "sample" : "<required>",
-        "scale" : 1,
-        "field_scale" : 2,
-        "threshold" : 500,
-        "mask" : "bone_region",
-        "mask_scale" : 2,
-        "verbose" : 2
-    })
+    argparser = default_parser(__doc__)
+    argparser = add_volume(argparser, 'field', 2)
+    argparser = add_volume(argparser, 'mask', 2)
+    argparser.add_argument('-t', '--threshold', action='store', type=int, default=500,
+        help='The threshold for the field. Default is 500.')
+    args = argparser.parse_args()
 
-    field_name = "edt"
     image_output_dir = f"{hdf5_root}/processed/bic/"
     os.makedirs(image_output_dir, exist_ok=True)
 
-    if verbose >= 1: print (f'Processing {sample} with threshold {threshold} and scales {scale}x, {field_scale}x, {mask_scale}x (voxels, field, mask)')
-    blood_file = h5py.File(f'{hdf5_root}/masks/{scale}x/{sample}.h5', 'r')
+    if args.verbose >= 1: print (f'Processing {args.sample} with threshold {args.threshold} and scales {args.sample_scale}x, {args.field_scale}x, {args.mask_scale}x (voxels, field, mask)')
+    blood_file = h5py.File(f'{hdf5_root}/masks/{args.sample_scale}x/{args.sample}.h5', 'r')
     blood = blood_file['blood']['mask']
     voxel_size = blood_file["implant"].attrs["voxel_size"]
-    field = np.load(f'{binary_root}/fields/implant-edt/{field_scale}x/{sample}.npy', mmap_mode='r')
-    mask_file = h5py.File(f'{hdf5_root}/masks/{mask_scale}x/{sample}.h5', 'r')
-    mask = mask_file[mask]['mask']
+    field = np.load(f'{binary_root}/fields/implant-{args.field}/{args.field_scale}x/{args.sample}.npy', mmap_mode='r')
+    mask_file = h5py.File(f'{hdf5_root}/masks/{args.mask_scale}x/{args.sample}.h5', 'r')
+    args.mask = mask_file[args.mask]['mask']
 
     # Compute the divisable shapes
-    assert blood.shape[0] >= field.shape[0] and blood.shape[0] >= mask.shape[0] # blood is the largest, handle others later
-    assert field.shape[0] >= mask.shape[0] # field is the largest, handle mask later
-    min_factor = max(field_scale, mask_scale) // scale
+    assert blood.shape[0] >= field.shape[0] and blood.shape[0] >= args.mask.shape[0] # blood is the largest, handle others later
+    assert field.shape[0] >= args.mask.shape[0] # field is the largest, handle mask later
+    min_factor = max(args.field_scale, args.mask_scale) // args.sample_scale
     bnz = (blood.shape[0] // min_factor) * min_factor
-    fnz = bnz // (field_scale // scale)
-    mnz = bnz // (mask_scale // scale)
+    fnz = bnz // (args.field_scale // args.sample_scale)
+    mnz = bnz // (args.mask_scale // args.sample_scale)
 
     blood = blood[:bnz]
     field = field[:fnz]
-    mask = mask[:mnz]
+    args.mask = args.mask[:mnz]
 
     blood_file.close()
     mask_file.close()
 
-    if verbose >= 2:
+    if args.verbose >= 2:
         print (f'Writing debug plane images to {image_output_dir}')
         nz, ny, nx = blood.shape
         names = ['yx', 'zx', 'zy']
@@ -64,20 +60,20 @@ if __name__ == '__main__':
 
         for name, plane in zip(names, planes):
             plt.imshow(plane)
-            plt.savefig(f"{image_output_dir}/{sample}_blood_{name}.png", bbox_inches='tight')
+            plt.savefig(f"{image_output_dir}/{args.sample}_blood_{name}.png", bbox_inches='tight')
             plt.clf()
 
         nz, ny, nx = field.shape
         planes = [field[nz//2,:,:], field[:,ny//2,:], field[:,:,nx//2]]
 
         for name, plane in zip(names, planes):
-            plt.imshow((plane < threshold) & (plane > 0))
-            plt.savefig(f"{image_output_dir}/{sample}_field_{name}.png", bbox_inches='tight')
+            plt.imshow((plane < args.threshold) & (plane > 0))
+            plt.savefig(f"{image_output_dir}/{args.sample}_field_{name}.png", bbox_inches='tight')
             plt.clf()
 
     bics = np.zeros(blood.shape[0], dtype=np.float32)
 
-    bic(blood, field, mask, threshold * scale, bics)
+    bic(blood, field, args.mask, args.threshold * args.sample_scale, bics)
 
-    plt.plot(bics); plt.savefig(f"{image_output_dir}/{sample}_bics.png", bbox_inches='tight'); plt.clf()
-    np.save(f"{image_output_dir}/{sample}_bics.npy", bics)
+    plt.plot(bics); plt.savefig(f"{image_output_dir}/{args.sample}_bics.png", bbox_inches='tight'); plt.clf()
+    np.save(f"{image_output_dir}/{args.sample}_bics.npy", bics)
