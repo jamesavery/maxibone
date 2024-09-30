3976c47c3942f2936bd33b7233819884a19267a0
Carl Johnsen <carl-johannes@di.ku.dk>
2024-09-30 20:43:21 +0200

    Fixed a bug with the default scale not being set for the sample in 1500

--- src/processing_steps/1500_segment_blood_cc.py
+++ src/processing_steps/1500_segment_blood_cc.py
@@ -25,7 +25,7 @@ import scipy.ndimage as ndi
 import tqdm
 
 if __name__ == '__main__':
-    argparser = default_parser(__doc__)
+    argparser = default_parser(__doc__, 0)
     argparser = add_volume(argparser, 'field')
     argparser.add_argument('-m', '--material', action='store', type=int, default=0,
         help='The material to segment. Default is 0, which should be soft tissue.')
