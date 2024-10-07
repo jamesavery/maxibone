afc1b696ff70bf6e6b7843da90993288c761e7d1
Carl Johnsen <carl-johannes@di.ku.dk>
2024-10-07 18:51:15 +0200

    #37 Fixed bug in largest_cc_of where the verbose argument for plotting was positional, when it should be named.

--- src/lib/py/helpers.py
+++ src/lib/py/helpers.py
@@ -718,8 +718,8 @@ def largest_cc_of(sample_name, scale, mask, mask_name, plotting, plotting_dir, v
         final_n_labels = lib_connected_components.merge_labeled_chunks(labels, np.array(n_labels), (n_chunks, layers_per_chunk, ny, nx), verbose)
 
         if plotting:
-            plot_middle_planes(labels, plotting_dir, 'labeled', verbose)
-            plot_middle_planes(labels > 0, plotting_dir, 'labeled_binary', verbose)
+            plot_middle_planes(labels, plotting_dir, 'labeled', verbose=verbose)
+            plot_middle_planes(labels > 0, plotting_dir, 'labeled_binary', verbose=verbose)
 
         # TODO use lib_general.bincount at some point.
         bincounts = np.bincount(labels[labels > 0], minlength=final_n_labels+1)
